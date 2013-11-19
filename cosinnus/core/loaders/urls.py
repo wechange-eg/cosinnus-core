30b4958b83f9d5556751813614afeb97d263efc8
Markus Holtermann <info@markusholtermann.eu>
2013-11-19 18:13:04 +0100

    Fixed #4 -- Use separate url configurations for root urls and grouped urls.

--- cosinnus/core/loaders/urls.py
+++ cosinnus/core/loaders/urls.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from re import escape as re_escape
+
 from django.conf.urls import include, patterns, url
 from django.utils.importlib import import_module
 
@@ -23,9 +25,18 @@ class CosinnusSite(object):
             if getattr(module, 'IS_COSINNUS_APP', False):
                 app_name = getattr(module, 'COSINNUS_APP_NAME', app)
                 urls = import_module('%s.urls' % app)
-                self._urlpatterns += patterns('',
-                    url(r'', include(urls.urlpatterns, namespace=app_name))
-                )
+                root_patterns = getattr(urls, 'cosinnus_root_patterns', None)
+                if root_patterns:
+                    self._urlpatterns += patterns('',
+                        url(r'', include(root_patterns, namespace=app_name))
+                    )
+                group_patterns = getattr(urls, 'cosinnus_group_patterns', None)
+                if group_patterns:
+                    url_app_name = re_escape(app_name)
+                    url_base = r'^group/(?P<group>\d+)/%s/' % url_app_name
+                    self._urlpatterns += patterns('',
+                        url(url_base, include(group_patterns, namespace=app_name)),
+                    )
 
     @property
     def urlpatterns(self):
