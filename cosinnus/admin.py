5c5fcb6b63fabae93c26254993a4bd62c4d746e3
Markus Holtermann <info@markusholtermann.eu>
2013-11-20 20:09:17 +0100

    Add admin for User and UserProfile

--- cosinnus/admin.py
+++ cosinnus/admin.py
@@ -2,11 +2,15 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from django.contrib.auth.admin import Group, GroupAdmin as DjangoGroupAdmin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import Group, GroupAdmin as DjangoGroupAdmin, UserAdmin as DjangoUserAdmin
 
 from cosinnus.forms.group import GroupForm
 from cosinnus.models.group import GroupAdmin
+from cosinnus.models.profile import get_user_profile_model
 
+
+## group related admin
 
 class GroupAdminAdmin(admin.ModelAdmin):
     list_display = ('group', 'user')
@@ -26,3 +30,25 @@ class GroupAdmin(DjangoGroupAdmin):
 
 admin.site.unregister(Group)
 admin.site.register(Group, GroupAdmin)
+
+
+## user / user profile related admin
+
+USER_PROFILE_MODEL = get_user_profile_model()
+USER_MODEL = get_user_model()
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(get_user_profile_model(), UserProfileAdmin)
+
+
+class UserProfileInline(admin.StackedInline):
+    model = USER_PROFILE_MODEL
+
+
+class UserAdmin(DjangoUserAdmin):
+    inlines = (UserProfileInline,)
+
+admin.site.unregister(USER_MODEL)
+admin.site.register(USER_MODEL, UserAdmin)
