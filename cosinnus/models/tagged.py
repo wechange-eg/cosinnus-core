9db6db19303338eea615d69985284ede1e10ac0d
Markus Holtermann <info@markusholtermann.eu>
2013-11-20 19:45:14 +0100

    Add FK to group to the BaseTaggableObjectModel

--- cosinnus/models/tagged.py
+++ cosinnus/models/tagged.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import Group
 from django.db import models
 from taggit.managers import TaggableManager
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from cosinnus.conf import settings
@@ -22,10 +24,14 @@ class PeopleModelMixin(models.Model):
         abstract = True
 
 
+@python_2_unicode_compatible
 class BaseTagObject(LocationModelMixin, PeopleModelMixin):
 
     class Meta:
         abstract = True
+
+    def __str__(self):
+        return "Tag object {0}".format(self.pk)
 
 
 class TagObject(BaseTagObject):
@@ -35,6 +41,7 @@ class TagObject(BaseTagObject):
         swappable = 'COSINNUS_TAG_OBJECT_MODEL'
 
 
+@python_2_unicode_compatible
 class BaseTaggableObjectModel(models.Model):
     """
     Represents the base for all cosinnus main models. Each inheriting model
@@ -43,8 +50,15 @@ class BaseTaggableObjectModel(models.Model):
     people and so on.
     """
 
-    tags = TaggableManager(_(u'Tags'), blank=True)
-    media_tag = models.OneToOneField(settings.COSINNUS_TAG_OBJECT_MODEL, blank=True, null=True)
+    tags = TaggableManager(_('Tags'), blank=True)
+    media_tag = models.OneToOneField(settings.COSINNUS_TAG_OBJECT_MODEL,
+        blank=True, null=True, on_delete=models.PROTECT)
+
+    group = models.ForeignKey(Group, verbose_name=_('Group'),
+        related_name='%(app_label)s_%(class)s_set', on_delete=models.PROTECT)
 
     class Meta:
         abstract = True
+
+    def __str__(self):
+        return "Tagged object {0}".format(self.pk)
