9d33bea573f2d7bf5004f17473cece6426442fc0
Markus Holtermann <info@markusholtermann.eu>
2013-11-25 16:24:50 +0100

    Provide a test infrastructure
    Install tox (`$ pip install tox`) and run `$ tox`.

---
+++ tests/settings/base.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import django
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+TIME_ZONE = 'Europe/Berlin'
+LANGUAGE_CODE = 'en-us'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+SECRET_KEY = 'test-secret-key'
+ROOT_URLCONF = 'cosinnus.urls'
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'taggit',
+    'cosinnus',
+)
+if django.VERSION[:2] < (1, 6):
+    TEST_RUNNER = 'discover_runner.DiscoverRunner'
