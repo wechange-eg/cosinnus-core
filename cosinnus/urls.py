fedb48fac4c1c089f3e83fc54f158ccec29f7b6f
Markus Holtermann <info@markusholtermann.eu>
2013-12-02 17:01:38 +0100

    Fixed #22, #23 -- Add login, logout, profile and group list to top menu

--- cosinnus/urls.py
+++ cosinnus/urls.py
@@ -6,7 +6,22 @@ from django.conf.urls import patterns, url
 from cosinnus.core.loaders.urls import cosinnus_site
 
 
-urlpatterns = patterns('cosinnus.views',
+urlpatterns = patterns('django.contrib.auth.views',
+    url(r'^login/$', 'login', {'template_name': 'cosinnus/login.html'}, name='login'),
+    url(r'^logout/$', 'logout', {'template_name': 'cosinnus/logged_out.html'}, name='logout'),
+
+    # TODO: adjust templates
+    url(r'^password_change/$', 'password_change', name='password_change'),
+    url(r'^password_change/done/$', 'password_change_done', name='password_change_done'),
+    url(r'^password_reset/$', 'password_reset', name='password_reset'),
+    url(r'^password_reset/done/$', 'password_reset_done', name='password_reset_done'),
+    url(r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        'password_reset_confirm',
+        name='password_reset_confirm'),
+    url(r'^reset/done/$', 'password_reset_complete', name='password_reset_complete'),
+)
+
+urlpatterns += patterns('cosinnus.views',
     url(r'^$', 'common.index', name='index'),
 
     url(r'^profile/$', 'profile.detail_view', name='profile-detail'),
