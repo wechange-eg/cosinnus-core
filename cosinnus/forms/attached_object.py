249aa7533a389eaf742de4423c9b799fcffb0151
Sascha Narr <saschanarr@googlemail.com>
2013-12-11 19:25:08 +0100

    Adding attached objects now working!

--- cosinnus/forms/attached_object.py
+++ cosinnus/forms/attached_object.py
@@ -1,29 +1,48 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django import forms
+from cosinnus.models.tagged import AttachedObject
+from django.contrib.contenttypes.models import ContentType
 
 class FormAttachable(forms.ModelForm):
-    
+
     def __init__(self, *args, **kwargs):
-        attached_sets = kwargs.pop('attached_files_querysets', None) 
+        attached_sets = kwargs.pop('attached_files_querysets', None)
         super(FormAttachable, self).__init__(*args, **kwargs)
-        
+
         print(">>> query_sets passed forward:")
         print(attached_sets)
-        
+
         for model_name, queryset in attached_sets.items():
-            self.fields[model_name] = forms.ModelMultipleChoiceField(queryset=queryset, required=False)#,label='FFFFFUUUU')
-    
-    def save(self, *args, **kwargs):
-        instance = super(FormAttachable, self).save(*args, **kwargs)
-        
+            self.fields[model_name] = forms.ModelMultipleChoiceField(queryset=queryset, required=False)  # ,label='FFFFFUUUU')
+            """ TODO: add initial """
+            # self.initial[model_name] = <file.pk of the files that are linked by the instances attached_objects>
+
+
+    def save_attachable(self):
+
         print (">>> Now in the save func: instance is:")
-        print(instance)
+        print(self.instance)
         print (">>> cleaned data")
         print(self.cleaned_data)
-        
+
+
         """ TODO: save attached_objects to instance """
-        
-        
-        return instance
-        
+       # import ipdb; ipdb.set_trace();
+
+        for key, entries in self.cleaned_data.items():
+            if key.startswith('attached_cosinnus'):
+                for attached_obj in entries:
+                    print(">>> Found a fitting object with key " + key)
+                    print(attached_obj)
+                    object_id = str(attached_obj.pk)
+                    content_type = ContentType.objects.get_for_model(attached_obj)
+
+                    print(object_id)
+                    print(content_type)
+                    (ao, created) = AttachedObject.objects.get_or_create(content_type=content_type, object_id=object_id)
+                    print(">>> Finding the linked object, we got back: " + str(created))
+                    print(ao)
+                    self.instance.attached_objects.add(ao)
+                    print("instance saved!")
+
--- cosinnus/views/attached_object.py
+++ cosinnus/views/attached_object.py
@@ -5,6 +5,7 @@ from django.views.generic.edit import CreateView, UpdateView
 from django.db.models.loading import get_model
 
 from cosinnus.core.loaders.attached_objects import cosinnus_attached_object_registry as caor
+from django.http.response import HttpResponseRedirect
 
 '''
 Created on 11.12.2013
@@ -14,43 +15,47 @@ Created on 11.12.2013
 
 
 class AttachableViewMixin(object):
-    
+
     def get_form_kwargs(self):
-        kwargs = self.super_view.get_form_kwargs(self)
-        
+        kwargs = super(AttachableViewMixin, self).get_form_kwargs()
+
         print(kwargs)
         source_model_id = self.model._meta.app_label + '.' + self.model._meta.object_name
-        #self.model
-        
+        # self.model
+
         attachable_objects = caor.attachable_to.get(source_model_id, [])
-        
+
         print(">>> Attachable objects:")
         print(attachable_objects)
-        
+
         querysets = dict()
-        
-        #import ipdb; ipdb.set_trace();
+
+        # import ipdb; ipdb.set_trace();
         for attach_model_id in attachable_objects:
-            #attach_model_name ="cosinnus_file.FileEntry"
+            # attach_model_name ="cosinnus_file.FileEntry"
             app_label, model_name = attach_model_id.split('.')
             attach_model_class = get_model(app_label, model_name)
             query_set = attach_model_class._default_manager.filter(group=self.group)
-            
+
             print(">>> model_class")
             print(attach_model_class)
             print(query_set)
-            querysets['attached_' + attach_model_id.replace('.', '_')] = query_set
-            #import ipdb; ipdb.set_trace();
-        
-        
-        
+            querysets['attached_' + attach_model_id.replace('.', '.')] = query_set
+            # import ipdb; ipdb.set_trace();
+
+
         kwargs.update({'attached_files_querysets': querysets})
         return kwargs
 
+    def form_valid(self, form):
+        form.save()
+        if hasattr(form, 'save_m2m'):
+            form.save_m2m()
+        form.save_attachable()
+        return HttpResponseRedirect(self.get_success_url())
 
 class CreateViewAttachable(AttachableViewMixin, CreateView):
-    super_view = CreateView
+    pass
 
 class UpdateViewAttachable(AttachableViewMixin, UpdateView):
-    super_view = UpdateView
-    
+    pass
