b2be213612b0182233324ab27bbe05497d0af70f
Markus Holtermann <info@markusholtermann.eu>
2013-12-13 14:54:21 +0100

    Add some comments along the CosinnusGroup cachig

--- cosinnus/models/group.py
+++ cosinnus/models/group.py
@@ -15,8 +15,13 @@ from cosinnus.conf import settings
 from cosinnus.utils.functions import unique_aware_slugify
 
 
+#: Role defining a user has requested to be added to a group
 MEMBERSHIP_PENDING = 0
+
+#: Role defining a user is a member but not an admin of a group
 MEMBERSHIP_MEMBER = 1
+
+#: Role defining a user is an admin of a group
 MEMBERSHIP_ADMIN = 2
 
 MEMBERSHIP_STATUSES = (
@@ -25,12 +30,14 @@ MEMBERSHIP_STATUSES = (
     (MEMBERSHIP_ADMIN, p_('cosinnus membership status', 'admin')),
 )
 
+#: A user is a member of a group if either is an explicit member or admin
 MEMBER_STATUS = (MEMBERSHIP_MEMBER, MEMBERSHIP_ADMIN,)
 
 
 _GROUPS_SLUG_CACHE_KEY = 'cosinnus/core/groups/slugs'
-_GROUPS_ID_CACHE_KEY = 'cosinnus/core/groups/ids'
+_GROUPS_PK_CACHE_KEY = 'cosinnus/core/groups/pks'
 _GROUP_CACHE_KEY = 'cosinnus/core/group/%s'
+
 _MEMBERSHIP_ADMINS_KEY = 'cosinnus/core/membership/admins/%d'
 _MEMBERSHIP_MEMBERS_KEY = 'cosinnus/core/membership/members/%d'
 _MEMBERSHIP_PENDINGS_KEY = 'cosinnus/core/membership/pendings/%d'
@@ -44,32 +51,90 @@ def group_name_validator(value):
     )(value)
 
 
+class CosinnusGroupQS(models.query.QuerySet):
+
+    def filter_membership_status(self, status):
+        if isinstance(status, (list, tuple)):
+            return self.filter(memberships__status__in=status)
+        return self.filter(memberships__status=status)
+
+    def update(self, **kwargs):
+        ret = super(CosinnusGroupQS, self).update(**kwargs)
+        self.model._clear_cache()
+        return ret
+
+
+class CosinnusGroupMembershipQS(models.query.QuerySet):
+
+    def filter_membership_status(self, status):
+        if isinstance(status, (list, tuple)):
+            return self.filter(status__in=status)
+        return self.filter(status=status)
+
+    def update(self, **kwargs):
+        ret = super(CosinnusGroupMembershipQS, self).update(**kwargs)
+        self.model._clear_cache()
+        return ret
+
+
 class CosinnusGroupManager(models.Manager):
 
     use_for_related_fields = True
 
+    def get_query_set(self):
+        return CosinnusGroupQS(self.model, using=self._db)
+
+    def filter_membership_status(self, status):
+        return self.get_query_set().filter_membership_status(status)
+
     def get_slugs(self):
+        """
+        Gets all group slugs from the cache or, if the can has not been filled,
+        gets the slugs and pks from the database and fills the cache.
+
+        :returns: A :class:`SortedDict` with a `slug => pk` mapping of all
+            groups
+        """
         slugs = cache.get(_GROUPS_SLUG_CACHE_KEY)
         if slugs is None:
             slugs = SortedDict(self.values_list('slug', 'id').all())
             ids = SortedDict((v, k) for k, v in six.iteritems(slugs))
             cache.set(_GROUPS_SLUG_CACHE_KEY, slugs)
-            cache.set(_GROUPS_ID_CACHE_KEY, ids)
+            cache.set(_GROUPS_PK_CACHE_KEY, ids)
         return slugs
 
-    def get_ids(self):
-        ids = cache.get(_GROUPS_ID_CACHE_KEY)
+    def get_pks(self):
+        """
+        Gets all group pks from the cache or, if the can has not been filled,
+        gets the pks and slugs from the database and fills the cache.
+
+        :returns: A :class:`SortedDict` with a `pk => slug` mapping of all
+            groups
+        """
+        ids = cache.get(_GROUPS_PK_CACHE_KEY)
         if ids is None:
             ids = SortedDict(self.values_list('id', 'slug').all())
             slugs = SortedDict((v, k) for k, v in six.iteritems(ids))
-            cache.set(_GROUPS_ID_CACHE_KEY, ids)
+            cache.set(_GROUPS_PK_CACHE_KEY, ids)
             cache.set(_GROUPS_SLUG_CACHE_KEY, slugs)
         return ids
 
-    def get_cached(self, slugs=None, ids=None):
+    def get_cached(self, slugs=None, pks=None):
+        """
+        Gets all groups defined by either `slugs` or `pks`.
+
+        `slugs` and `pks` may be a list or tuple of identifiers to use for
+        request where the elements are of type string / unicode or int,
+        respectively. You may provide a single string / unicode or int directly
+        to query only one object.
+
+        :returns: An instance or a list of instances of :class:`CosinnusGroup`.
+        :raises: If a single object is defined a `CosinnusGroup.DoesNotExist`
+            will be raised in case the requested object does not exist.
+        """
         # Check that only one of slug and slugs is set:
-        assert not (slugs and ids)
-        if slugs is None and ids is None:
+        assert not (slugs and pks)
+        if slugs is None and pks is None:
             slugs = list(self.get_slugs().keys())
 
         if slugs:
@@ -93,17 +158,17 @@ class CosinnusGroupManager(models.Manager):
                     cache.set_many(groups)
                 return sorted(groups.values(), key=lambda x: x.name)
         else:
-            if isinstance(slugs, int):
+            if isinstance(pks, int):
                 # We request a single group
-                cached_ids = self.get_ids()
-                slug = cached_ids.get(ids, None)
+                cached_pks = self.get_pks()
+                slug = cached_pks.get(pks, None)
                 if slug:
                     return self.get_cached(slugs=slug)
                 return None  # We rely on the slug and id maps being up to date
             else:
                 # We request multiple groups
-                cached_ids = self.get_ids()
-                slugs = filter(None, (cached_ids.get(id, None) for id in ids))
+                cached_pks = self.get_pks()
+                slugs = filter(None, (cached_pks.get(id, None) for id in pks))
                 if slugs:
                     return self.get_cached(slugs=slugs)
                 return []  # We rely on the slug and id maps being up to date
@@ -112,11 +177,19 @@ class CosinnusGroupManager(models.Manager):
         return self.get_cached(slugs=slug)
 
     def get_for_user(self, user):
-        ids = self.filter(memberships__status__in=MEMBER_STATUS,
-                          memberships__user_id=user.pk).values_list('id', flat=True)
-        return self.get_cached(ids=ids)
+        """
+        :returns: a list of :class:`CosinnusGroup` the given user is a member
+            or admin of.
+        """
+        pks = self.filter(memberships__user_id=user.pk) \
+                  .filter_membership_status(MEMBER_STATUS) \
+                  .values_list('id', flat=True)
+        return self.get_cached(pks=pks)
 
     def public(self):
+        """
+        :returns: An iterator over all public groups.
+        """
         for group in self.get_cached():
             if group.public:
                 yield group
@@ -126,14 +199,16 @@ class CosinnusGroupMembershipManager(models.Manager):
 
     use_for_related_fields = True
 
+    def get_query_set(self):
+        return CosinnusGroupMembershipQS(self.model, using=self._db)
+
+    def filter_membership_status(self, status):
+        return self.get_query_set().filter_membership_status(status)
+
     def _get_users_for_single_group(self, group_id, cache_key, status):
         uids = cache.get(cache_key % group_id)
         if uids is None:
-            query = self.filter(group_id=group_id)
-            if isinstance(status, (list, tuple)):
-                query = query.filter(status__in=status)
-            else:
-                query = query.filter(status=status)
+            query = self.filter(group_id=group_id).filter_membership_status(status)
             uids = list(query.values_list('user_id', flat=True).all())
             cache.set(cache_key % group_id, uids)
         return uids
@@ -143,11 +218,7 @@ class CosinnusGroupMembershipManager(models.Manager):
         users = cache.get_many(keys)
         missing = map(int, (key.split('/')[-1] for key in keys if key not in users))
         if missing:
-            _q = self.get_query_set().values_list('user_id', flat=True)
-            if isinstance(status, (list, tuple)):
-                _q = _q.filter(status__in=status)
-            else:
-                _q = _q.filter(status=status)
+            _q = self.filter_membership_status(status).values_list('user_id', flat=True)
             for group in missing:
                 uids = list(_q._clone().filter(group_id=group).all())
                 users[cache_key % group] = uids
@@ -155,6 +226,10 @@ class CosinnusGroupMembershipManager(models.Manager):
         return dict((int(k.split('/')[-1]), v) for k, v in six.iteritems(users))
 
     def get_admins(self, group=None, groups=None):
+        """
+        Given either a group or a list of groups, this function returns all
+        members with the :data:`MEMBERSHIP_ADMIN` role.
+        """
         assert (group is None) ^ (groups is None)
         if group:
             gid = isinstance(group, int) and group or group.pk
@@ -164,6 +239,10 @@ class CosinnusGroupMembershipManager(models.Manager):
             return self._get_users_for_multiple_groups(gids, _MEMBERSHIP_ADMINS_KEY, MEMBERSHIP_ADMIN)
 
     def get_members(self, group=None, groups=None):
+        """
+        Given either a group or a list of groups, this function returns all
+        members with the :data:`MEMBERSHIP_MEMBER` role.
+        """
         assert (group is None) ^ (groups is None)
         if group:
             gid = isinstance(group, int) and group or group.pk
@@ -173,6 +252,10 @@ class CosinnusGroupMembershipManager(models.Manager):
             return self._get_users_for_multiple_groups(gids, _MEMBERSHIP_MEMBERS_KEY, MEMBER_STATUS)
 
     def get_pendings(self, group=None, groups=None):
+        """
+        Given either a group or a list of groups, this function returns all
+        members with the :data:`MEMBERSHIP_PENDING` role.
+        """
         assert (group is None) ^ (groups is None)
         if group:
             gid = isinstance(group, int) and group or group.pk
@@ -209,8 +292,8 @@ class CosinnusGroup(models.Model):
         return self.name
 
     def delete(self, *args, **kwargs):
+        super(CosinnusGroup, self).delete(*args, **kwargs)
         self._clear_cache()
-        super(CosinnusGroupMembership, self).delete(*args, **kwargs)
 
     def save(self, *args, **kwargs):
         slugs = [self.slug] if self.slug else []
@@ -220,18 +303,21 @@ class CosinnusGroup(models.Model):
         self._clear_cache(slug=self.slug)
 
     def is_admin(self, user):
+        """Checks whether the given user is an admin of this group"""
         if self._admins is None:
             self._admins = CosinnusGroupMembership.objects.get_admins(self.pk)
         uid = isinstance(user, int) and user or user.pk
         return uid in self._admins
 
     def is_member(self, user):
+        """Checks whether the given user is a member of this group"""
         if self._members is None:
             self._members = CosinnusGroupMembership.objects.get_members(self.pk)
         uid = isinstance(user, int) and user or user.pk
         return uid in self._members
 
     def is_pending(self, user):
+        """Checks whether the given user has a pending status on this group"""
         if self._pendings is None:
             self._pendings = CosinnusGroupMembership.objects.get_pendings(self.pk)
         uid = isinstance(user, int) and user or user.pk
@@ -242,7 +328,7 @@ class CosinnusGroup(models.Model):
         # TODO: clear membership caches
         keys = [
             _GROUPS_SLUG_CACHE_KEY,
-            _GROUPS_ID_CACHE_KEY,
+            _GROUPS_PK_CACHE_KEY,
         ]
         if slug:
             keys.append(_GROUP_CACHE_KEY % slug)
@@ -280,8 +366,8 @@ class CosinnusGroupMembership(models.Model):
         }
 
     def delete(self, *args, **kwargs):
-        self._clear_cache()
         super(CosinnusGroupMembership, self).delete(*args, **kwargs)
+        self._clear_cache()
 
     def save(self, *args, **kwargs):
         # Only update the date if the the state changes from pending to member
