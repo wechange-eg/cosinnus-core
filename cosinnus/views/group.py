2ff84c743c6bf4ef7b2b90f336bcd044fbbba7c5
Markus Holtermann <info@markusholtermann.eu>
2013-12-14 21:32:39 +0100

    Add views to join, withdraw and leave a group

--- cosinnus/views/group.py
+++ cosinnus/views/group.py
@@ -2,17 +2,21 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth import get_user_model
-from django.core.urlresolvers import reverse
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ImproperlyConfigured
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
+from django.utils.encoding import force_text
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic import (CreateView, DeleteView, DetailView, FormView,
     ListView, UpdateView)
 
 from cosinnus.core.decorators.views import superuser_required
 from cosinnus.forms.group import CosinnusGroupForm
-from cosinnus.models import CosinnusGroup, CosinnusGroupMembership
+from cosinnus.models import (CosinnusGroup, CosinnusGroupMembership,
+    MEMBERSHIP_PENDING, MEMBERSHIP_MEMBER)
 from cosinnus.views.mixins.group import RequireAdminMixin, RequireReadMixin
 
 
@@ -137,12 +141,126 @@ class GroupUserListView(RequireReadMixin, ListView):
 group_user_list = GroupUserListView.as_view()
 
 
-class GroupUserAddView(RequireAdminMixin, FormView):
-    # TODO: I don't like this solution yet. Even though enforcing POST
-    # requests, I think I'm missing some security concerns. We'd better use a
-    # FormView
+class GroupConfirmMixin(object):
 
-    http_method_names = ['post']
+    model = CosinnusGroup
+    slug_url_kwarg = 'group'
+    success_url = reverse_lazy('cosinnus:group-list')
+
+    def get_context_data(self, **kwargs):
+        context = super(GroupConfirmMixin, self).get_context_data(**kwargs)
+        context.update({
+            'confirm_label': self.get_confirm_label(),
+            'confirm_question': self.get_confirm_question(),
+            'confirm_title': self.get_confirm_title(),
+            'submit_css_classes': getattr(self, 'submit_css_classes', None)
+        })
+        return context
+
+    def post(self, *args, **kwargs):
+        self.object = self.get_object()
+        self.confirm_action()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        """
+        Returns the supplied success URL.
+        """
+        if self.success_url:
+            # Forcing possible reverse_lazy evaluation
+            url = force_text(self.success_url)
+        else:
+            raise ImproperlyConfigured(
+                "No URL to redirect to. Provide a success_url.")
+        return url
+
+    def confirm_action(self):
+        raise NotImplementedError()
+
+    def get_confirm_label(self):
+        return self.confirm_label
+
+    def get_confirm_question(self):
+        return self.confirm_question % {'group_name': self.object.name}
+
+    def get_confirm_title(self):
+        return self.confirm_title % {'group_name': self.object.name}
+
+
+class GroupUserJoinView(GroupConfirmMixin, DetailView):
+
+    confirm_label = _('Join')
+    confirm_question = _('Do you want to join the group “%(group_name)s”?')
+    confirm_title = _('Join group “%(group_name)s”?')
+    template_name = 'cosinnus/group_confirm.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(GroupUserJoinView, self).dispatch(request, *args, **kwargs)
+
+    def confirm_action(self):
+        CosinnusGroupMembership.objects.create(
+            user=self.request.user,
+            group=self.object,
+            status=MEMBERSHIP_PENDING
+        )
+
+group_user_join = GroupUserJoinView.as_view()
+
+
+class GroupUserLeaveView(GroupConfirmMixin, DetailView):
+
+    confirm_label = _('Leave')
+    confirm_question = _('Do you want to leave the group “%(group_name)s”?')
+    confirm_title = _('Leaving group “%(group_name)s”?')
+    submit_css_classes = 'btn-danger'
+    template_name = 'cosinnus/group_confirm.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(GroupUserLeaveView, self).dispatch(request, *args, **kwargs)
+
+    def confirm_action(self):
+        try:
+            membership = CosinnusGroupMembership.objects.get(
+                user=self.request.user,
+                group=self.object,
+                status=MEMBERSHIP_MEMBER
+            )
+            membership.delete()
+        except CosinnusGroupMembership.DoesNotExist:
+            pass
+
+group_user_leave = GroupUserLeaveView.as_view()
+
+
+class GroupUserWithdrawView(GroupConfirmMixin, DetailView):
+
+    confirm_label = _('Withdraw')
+    confirm_question = _('Do you want to withdraw your join request to the group “%(group_name)s”?')
+    confirm_title = _('Withdraw join request to group “%(group_name)s”?')
+    submit_css_classes = 'btn-danger'
+    template_name = 'cosinnus/group_confirm.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(GroupUserWithdrawView, self).dispatch(request, *args, **kwargs)
+
+    def confirm_action(self):
+        try:
+            membership = CosinnusGroupMembership.objects.get(
+                user=self.request.user,
+                group=self.object,
+                status=MEMBERSHIP_PENDING
+            )
+            membership.delete()
+        except CosinnusGroupMembership.DoesNotExist:
+            pass
+
+group_user_withdraw = GroupUserWithdrawView.as_view()
+
+
+class GroupUserAddView(RequireAdminMixin, FormView):
 
     def post(self, request, *args, **kwargs):
         user = get_object_or_404(get_user_model(), username=kwargs.get('username'))
