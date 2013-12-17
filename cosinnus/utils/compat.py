3c2af740ce6f3d670960f565538dfda81cc6015c
Markus Holtermann <info@markusholtermann.eu>
2013-12-18 00:57:42 +0100

    Fixed #17 -- Add Django compat module (cosinnus.utils.compat)

---
+++ cosinnus/utils/compat.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+"""
+.. warning::
+
+    This file's imports are not guaranteed to be fully backwards functional.
+    When Django core changed something that does not offer a simple deprecation
+    we may use functions from here.
+"""
+
+try:
+    from django.db.transaction import atomic
+except ImportError:
+    from django.db.transaction import commit_on_success as atomic  # noqa
+
+__all__ = ['atomic']
