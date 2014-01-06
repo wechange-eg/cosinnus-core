17340ed391a7dba6e2aa5e63c527c3f34cd2fce7
Markus Holtermann <info@markusholtermann.eu>
2014-01-06 17:00:06 +0100

    Change CosinnusSite to inherit from BaseRegistry in order to make a migration to Django 1.7 easier

--- cosinnus/core/loaders/urls.py
+++ cosinnus/core/loaders/urls.py
@@ -6,42 +6,34 @@ from re import escape as re_escape
 from django.conf.urls import include, patterns, url
 from django.utils.importlib import import_module
 
-from cosinnus.conf import settings
+from cosinnus.core.loaders.registry import BaseRegistry
 
 
-class CosinnusSite(object):
+class CosinnusSite(BaseRegistry):
 
-    def __init__(self):
-        self.ready = False
+    def __init__(self, module_name):
+        super(CosinnusSite, self).__init__(module_name)
         self._urlpatterns = patterns('')
 
-    def autodiscover(self):
-        self.ready = True
-        for app in settings.INSTALLED_APPS:
-            try:
-                module = import_module('%s.cosinnus_app' % app)
-            except ImportError:
-                continue
-            if getattr(module, 'IS_COSINNUS_APP', False):
-                app_name = getattr(module, 'COSINNUS_APP_NAME', app)
-                urls = import_module('%s.urls' % app)
-                root_patterns = getattr(urls, 'cosinnus_root_patterns', None)
-                if root_patterns:
-                    self._urlpatterns += patterns('',
-                        url(r'', include(root_patterns, namespace=app_name, app_name=app))
-                    )
-                group_patterns = getattr(urls, 'cosinnus_group_patterns', None)
-                if group_patterns:
-                    url_app_name = re_escape(app_name)
-                    url_base = r'^group/(?P<group>[^/]+)/%s/' % url_app_name
-                    self._urlpatterns += patterns('',
-                        url(url_base, include(group_patterns, namespace=app_name, app_name=app)),
-                    )
+    def setup_actions(self, app, module):
+        if getattr(module, 'IS_COSINNUS_APP', False):
+            app_name = getattr(module, 'COSINNUS_APP_NAME', app)
+            urls = import_module('%s.urls' % app)
+            root_patterns = getattr(urls, 'cosinnus_root_patterns', None)
+            if root_patterns:
+                self._urlpatterns += patterns('',
+                    url(r'', include(root_patterns, namespace=app_name, app_name=app))
+                )
+            group_patterns = getattr(urls, 'cosinnus_group_patterns', None)
+            if group_patterns:
+                url_app_name = re_escape(app_name)
+                url_base = r'^group/(?P<group>[^/]+)/%s/' % url_app_name
+                self._urlpatterns += patterns('',
+                    url(url_base, include(group_patterns, namespace=app_name, app_name=app)),
+                )
 
     @property
     def urlpatterns(self):
-        if not self.ready:
-            self.autodiscover_cosinnus_apps()
         return self._urlpatterns
 
-cosinnus_site = CosinnusSite()
+cosinnus_site = CosinnusSite('cosinnus_app')
