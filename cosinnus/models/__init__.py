5d3c052ca9667e507f5e35515c5372b6ddd94e58
Markus Holtermann <info@markusholtermann.eu>
2014-01-08 19:58:15 +0100

    Refers #2, #3 -- Add widget config models

--- cosinnus/models/__init__.py
+++ cosinnus/models/__init__.py
@@ -3,6 +3,7 @@
 from cosinnus.models.group import *  # noqa
 from cosinnus.models.profile import *  # noqa
 from cosinnus.models.tagged import *  # noqa
+from cosinnus.models.widget import *  # noqa
 
 from cosinnus.core.loaders.apps import cosinnus_app_registry
 cosinnus_app_registry.autodiscover()
