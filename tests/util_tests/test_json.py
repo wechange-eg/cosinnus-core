cd6fd34392f8053a93eb9659c284b21adc8cd2c5
Markus Holtermann <info@markusholtermann.eu>
2014-03-06 00:48:45 +0100

    fix test, HttpResponse.content is a byte string

--- tests/util_tests/test_json.py
+++ tests/util_tests/test_json.py
@@ -14,30 +14,30 @@ class JSONResponseTest(SimpleTestCase):
 
     def test_empty(self):
         r = JSONResponse({})
-        self.assertEqual(r.content, '{}')
+        self.assertEqual(r.content, b'{}')
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r['content-type'], self.content_type)
 
     def test_number(self):
         r = JSONResponse({'num': 1})
-        self.assertEqual(r.content, '{"num": 1}')
+        self.assertEqual(r.content, b'{"num": 1}')
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r['content-type'], self.content_type)
 
     def test_float(self):
         r = JSONResponse({'float': 1.23})
-        self.assertEqual(r.content, '{"float": 1.23}')
+        self.assertEqual(r.content, b'{"float": 1.23}')
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r['content-type'], self.content_type)
 
     def test_datetime(self):
         r = JSONResponse({'datetime': datetime.datetime(2014, 3, 4, 12, 34, 56)})
-        self.assertEqual(r.content, '{"datetime": "2014-03-04T12:34:56"}')
+        self.assertEqual(r.content, b'{"datetime": "2014-03-04T12:34:56"}')
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r['content-type'], self.content_type)
 
     def test_status_code(self):
         r = JSONResponse({'key': "val"}, status=404)
-        self.assertEqual(r.content, '{"key": "val"}')
+        self.assertEqual(r.content, b'{"key": "val"}')
         self.assertEqual(r.status_code, 404)
         self.assertEqual(r['content-type'], self.content_type)
