37f17007da91d6c6c86d248bd8e7f9256b240b61
Sascha Narr <saschanarr@googlemail.com>
2014-03-12 17:12:34 +0100

    Added global COSINNUS_USER context variable and js cosinnus_active_user variable

--- cosinnus/utils/context_processors.py
+++ cosinnus/utils/context_processors.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.core.urlresolvers import reverse
 
 from cosinnus.conf import settings as SETTINGS
+from cosinnus.models.serializers.profile import UserSimpleSerializer
+import json
 
 
 def settings(request):
@@ -18,6 +20,12 @@ def cosinnus(request):
         domain=request.get_host(),
         path=reverse('cosinnus:index')
     )
+    user = request.user
+    if user.is_authenticated():
+        user_json = json.dumps(UserSimpleSerializer(request.user).data)
+    else:
+        user_json = json.dumps(False)
     return {
         'COSINNUS_BASE_URL': base_url,
+        'COSINNUS_USER':  user_json,
     }
