3b712c36a45b15c246b40f1b2ef25450aad079cc
Markus Holtermann <info@markusholtermann.eu>
2014-03-20 01:18:51 +0100

    Add date/datetime/time formats for to JS header

--- cosinnus/utils/context_processors.py
+++ cosinnus/utils/context_processors.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.core.urlresolvers import reverse
+from django.utils.formats import get_format
 
 from cosinnus.conf import settings as SETTINGS
 from cosinnus.models.serializers.profile import UserSimpleSerializer
@@ -20,12 +21,17 @@ def cosinnus(request):
         domain=request.get_host(),
         path=reverse('cosinnus:index')
     )
+
     user = request.user
     if user.is_authenticated():
         user_json = json.dumps(UserSimpleSerializer(request.user).data)
     else:
         user_json = json.dumps(False)
+
     return {
         'COSINNUS_BASE_URL': base_url,
         'COSINNUS_USER': user_json,
+        'COSINNUS_DATE_FORMAT': get_format('COSINNUS_DATETIMEPICKER_DATE_FORMAT'),
+        'COSINNUS_DATETIME_FORMAT': get_format('COSINNUS_DATETIMEPICKER_DATETIME_FORMAT'),
+        'COSINNUS_TIME_FORMAT': get_format('COSINNUS_DATETIMEPICKER_TIME_FORMAT'),
     }
