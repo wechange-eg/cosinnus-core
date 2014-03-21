e9542f1c4c33f4c36008c98f870d106b28ca5c78
Markus Holtermann <info@markusholtermann.eu>
2014-03-21 13:40:34 +0100

    Fix TagSerializer

--- cosinnus/models/serializers/tagged.py
+++ cosinnus/models/serializers/tagged.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import six
+
 from rest_framework import serializers
 
 
@@ -10,18 +12,25 @@ __all__ = ('TagListSerializer', )
 class TagListSerializer(serializers.WritableField):
     # from http://pypede.wordpress.com/2013/07/06/using-django-rest-framework-with-tagged-items-django-taggit/
 
+    def _quote_string(self, tag):
+        # adapted from taggit.utils.edit_string_for_tags
+        name = tag.name
+        return ('"%s"' % name) if (',' in name or ' ' in name) else name
+
     def from_native(self, data):
-        if not isinstance(data, (list, tuple)):
-            raise ValueError("Expected a list of data")
-        return data
+        """
+        Deserialize primitives -> objects.
+        """
+        if isinstance(data, (list, tuple)):
+            return ', '.join(map(self._quote_string, data))
+        elif isinstance(data, six.text_type):
+            return data
+        raise ValueError("Expected a list of data")
 
     def to_native(self, obj):
-
-        def quote_string(tag):
-        # adapted from taggit.utils.edit_string_for_tags
-            name = tag.name
-            return ('"%s"' % name) if (',' in name or ' ' in name) else name
-
+        """
+        Serialize objects -> primitives.
+        """
         if not isinstance(obj, (list, tuple)):
-            return list(map(quote_string, obj.all()))
+            return list(map(self._quote_string, obj.all()))
         return obj
