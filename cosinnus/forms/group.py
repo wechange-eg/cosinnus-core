96e9acf27b96ce0ea44dc101c8d5c7016320a6ff
Markus Holtermann <info@markusholtermann.eu>
2014-04-08 17:40:28 +0200

    Set the group for models forms in the __init__

--- cosinnus/forms/group.py
+++ cosinnus/forms/group.py
@@ -19,11 +19,8 @@ class GroupKwargModelFormMixin(object):
     def __init__(self, *args, **kwargs):
         self.group = kwargs.pop('group', None)
         super(GroupKwargModelFormMixin, self).__init__(*args, **kwargs)
-
-    def save(self, *args, **kwargs):
         if hasattr(self.instance, 'group_id'):
             self.instance.group_id = self.group.id
-        return super(GroupKwargModelFormMixin, self).save(*args, **kwargs)
 
 
 class CosinnusGroupForm(forms.ModelForm):
