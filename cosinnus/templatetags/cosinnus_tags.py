7e72de8aab5c2f170205ed09e8a3aaddd6598cd6
Sascha Narr <saschanarr@googlemail.com>
2014-05-06 12:46:15 +0200

    Added leftnav button templating

--- cosinnus/templatetags/cosinnus_tags.py
+++ cosinnus/templatetags/cosinnus_tags.py
@@ -182,3 +182,29 @@ def url_optional(parser, token):
     urlnode = url_tag(parser, token)
     return URLNodeOptional(urlnode.view_name, urlnode.args, urlnode.kwargs,
         urlnode.asvar)
+    
+@register.tag(name='captureas')
+def do_captureas(parser, token):
+    """
+        Captures block content into template variables.
+        Usage:
+            {% captureas label %}{% trans "Posteingang" %}{% if unread_count %} <strong>({{ unread_count }})</strong>{% endif %}{% endcaptureas %}
+            {% include "cosinnus/leftnav_button.html" label=label  %}
+    """
+    try:
+        tag_name, args = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError("'captureas' node requires a variable name.")
+    nodelist = parser.parse(('endcaptureas',))
+    parser.delete_first_token()
+    return CaptureasNode(nodelist, args)
+
+class CaptureasNode(template.Node):
+    def __init__(self, nodelist, varname):
+        self.nodelist = nodelist
+        self.varname = varname
+
+    def render(self, context):
+        output = self.nodelist.render(context)
+        context[self.varname] = output
+        return ""
