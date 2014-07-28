432d1a1419a2a46faa517b9cb69f2c9476ee2235
Sascha Narr <saschanarr@googlemail.com>
2014-07-28 16:32:06 +0200

    Add autogrow to dashboard.ready

--- cosinnus/static/js/cosinnus.dashboard.js
+++ cosinnus/static/js/cosinnus.dashboard.js
@@ -220,10 +220,9 @@ $('.js-todo-link').on('click', function(e) {
                 holder.attr("data-app-name", jqXHR.getResponseHeader('X-Cosinnus-Widget-App-Name'));
                 holder.attr("data-widget-name", jqXHR.getResponseHeader('X-Cosinnus-Widget-Widget-Name'));
                 
-                
-                    
-                
                 $.cosinnus.renderMomentDataDate();
+                $.cosinnus.autogrow();
+
             }).fail(function(jqXHR, textStatus, errorThrown) {
                 var error = $('<div class="alert alert-danger">An error occurred while loading the widget. </div>');
                 var reload = $('<a href="#" class="alert-link">Reload</a>').bind("click", {
