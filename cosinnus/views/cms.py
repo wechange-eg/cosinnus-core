b9004ebbd58ed69aa321366bbab76829099ad2b7
Sascha Narr <saschanarr@googlemail.com>
2014-09-01 13:17:18 +0200

    Added proper edit mode to micropage

--- cosinnus/views/cms.py
+++ cosinnus/views/cms.py
@@ -11,12 +11,14 @@ from django.utils.encoding import force_text
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic import (CreateView, DeleteView, DetailView,
     ListView, UpdateView, TemplateView)
-from cosinnus.views.mixins.group import RequireReadMixin
+from cosinnus.views.mixins.group import RequireReadMixin, RequireWriteMixin
 from cosinnus.models.widget import WidgetConfig
 from cosinnus.models.group import CosinnusGroup
 from django.http.response import HttpResponseNotFound
 from cosinnus.models.cms import CosinnusMicropage
 from cosinnus.core.registries import widget_registry
+from cosinnus.core.decorators.views import redirect_to_403
+from cosinnus.utils.permissions import check_object_write_access
 
 class GroupMicrosite(TemplateView):
     """ TODO: Refactor-merge and unify this view to a mixin with DashboardMixin for groups,
@@ -56,11 +58,36 @@ class GroupMicrosite(TemplateView):
             'widgets': ids,
             'group': self.group,
             'widget_configs': widget_configs,
+            'edit_mode': False,
         })
         return super(GroupMicrosite, self).get_context_data(**kwargs)
 
     def get_filter(self):
         return {'group_id': self.group.pk, 'type': WidgetConfig.TYPE_MICROSITE}
     
-
 group_microsite = GroupMicrosite.as_view()
+
+        
+class GroupMicrositeEdit(GroupMicrosite, RequireWriteMixin):
+    
+    def dispatch(self, request, *args, **kwargs):
+        """ Assure write access to group """
+        group_name = kwargs.get(self.group_url_kwarg, None)
+        try:
+            group = CosinnusGroup.objects.get(slug=group_name)
+        except CosinnusGroup.DoesNotExist:
+            return HttpResponseNotFound(_("No group found with this name"))
+        self.group = group
+        if (check_object_write_access(self.group, request.user)):
+            return super(GroupMicrosite, self).dispatch(request, *args, **kwargs)
+        # Access denied, redirect to 403 page and and display an error message
+        return redirect_to_403(request)
+            
+    def get_context_data(self, **kwargs):
+        context = super(GroupMicrositeEdit, self).get_context_data(**kwargs)
+        context.update({
+            'edit_mode':True
+        })
+        return context
+    
+group_microsite_edit = GroupMicrositeEdit.as_view()
