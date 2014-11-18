0ee75929cd72990f39ec7a254e77429ce4a2c932
Sascha Narr <saschanarr@googlemail.com>
2014-11-18 11:01:20 +0100

    Adjusted mail templates for notification variables

--- cosinnus/cosinnus_notifications.py
+++ cosinnus/cosinnus_notifications.py
@@ -15,10 +15,16 @@ from cosinnus.core.signals import user_group_join_requested
     
     Both the mail and subject template will be provided with the following context items:
         :receiver django.auth.User who receives the notification mail
-        :source_user django.auth.User whose action caused the notification to trigger
+        :sender django.auth.User whose action caused the notification to trigger
+        :receiver_name Convenience, full name of the receiver
+        :sender_name Convenience, full name of the sender
         :object The object that was created/changed/modified and which the notification is about.
+        :object_url The url of the object, if defined by get_absolute_url()
+        :object_name The title of the object (only available if it is a BaseTaggableObject)
+        :group_name The name of the group the object is housed in (only available if it is a BaseTaggableObject)
         :site_name Current django site's name
         :domain_url The complete base domain needed to prefix URLs. (eg: 'http://sinnwerkstatt.com')
+        :notification_settings_url The URL to the cosinnus notification settings page.
         :site Current django site
         :protocol Current portocol, 'http' or 'https'
         
