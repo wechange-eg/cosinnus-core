51f7412291e7ce0f84036f75fda864990790cdad
Sascha Narr <saschanarr@googlemail.com>
2014-11-24 12:50:13 +0100

    Housekeeping URL only accessible for admins

--- cosinnus/views/housekeeping.py
+++ cosinnus/views/housekeeping.py
@@ -2,9 +2,18 @@
 from __future__ import unicode_literals
 from cosinnus.models.group import CosinnusGroup
 from cosinnus.utils.dashboard import create_initial_group_widgets
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseForbidden
+
 
 def housekeeping(request):
+    """ Do some integrity checks and corrections. 
+        Currently doing:
+            - Checking all groups and projects for missing widgets versus the default widget
+                settings and adding missing widgets
+    """
+    if not request.user.is_superuser:
+        return HttpResponseForbidden('Not authenticated')
+    
     groups = CosinnusGroup.objects.all()
     ret = ""
     for group in groups:
