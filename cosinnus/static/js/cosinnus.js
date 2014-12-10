e6f8446170e7d8e3497881ec7e04cf137ad3b59b
Sascha Narr <saschanarr@googlemail.com>
2014-12-10 11:14:47 +0100

    Added decodeEntities function to cosinnus.JS

--- cosinnus/static/js/cosinnus.js
+++ cosinnus/static/js/cosinnus.js
@@ -966,7 +966,28 @@
                 $('.hidden_text_shown[data-hidden-text-id='+hide_id+']').hide();
                 $(this).hide();
             });
-        }
+        },
+        
+        
+        decodeEntities : (function() {
+            // this prevents any overhead from creating the object each time
+            var element = document.createElement('div');
+        
+            function decodeHTMLEntities (str) {
+                if(str && typeof str === 'string') {
+                    // strip script/html tags
+                    str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
+                    str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
+                    element.innerHTML = str;
+                    str = element.textContent;
+                    element.textContent = '';
+                }
+        
+                return str;
+            }
+        
+            return decodeHTMLEntities;
+        })(),
 
 	};
 })( jQuery );
