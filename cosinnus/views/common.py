8f5f75185818f58dad3449fd51f9b13f1b1f572c
Sascha Narr <saschanarr@googlemail.com>
2015-03-24 18:41:18 +0100

    Added better 403, 404 and 500 pages

--- cosinnus/views/common.py
+++ cosinnus/views/common.py
@@ -7,11 +7,45 @@ from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
 
 from cosinnus.conf import settings
+from django.views.generic.base import TemplateView
+from django.shortcuts import render_to_response
+
+from cosinnus.utils.context_processors import cosinnus as cosinnus_context
 
 class IndexView(RedirectView):
     url = reverse_lazy('cosinnus:group-list')
 
 index = IndexView.as_view()
+
+"""
+class PermissionDeniedView(TemplateView):
+    template_name = '403.html'
+    
+view_403 = PermissionDeniedView.as_view()
+
+class NotFoundView(TemplateView):
+    template_name = '404.html'
+    
+view_404 = NotFoundView.as_view()
+"""
+
+def _get_bare_cosinnus_context(request):
+    context = {
+        'request': request,
+        'user': request.user,
+    }
+    context.update(cosinnus_context(request))
+    return context
+
+
+def view_403(request):
+    return render_to_response('cosinnus/common/403.html', _get_bare_cosinnus_context(request))
+
+def view_404(request):
+    return render_to_response('cosinnus/common/404.html', _get_bare_cosinnus_context(request))
+
+def view_500(request):
+    return render_to_response('cosinnus/common/500.html')
 
 
 class SwitchLanguageView(RedirectView):
