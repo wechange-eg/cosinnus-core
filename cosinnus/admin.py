9f48c595b7dc53b8e2ef02b4225d935db163e791
Sascha Narr <saschanarr@googlemail.com>
2015-07-29 12:39:49 +0200

    Add raw_id_field for user portal membership

--- cosinnus/admin.py
+++ cosinnus/admin.py
@@ -71,6 +71,7 @@ class PortalMembershipAdmin(admin.ModelAdmin):
     list_display = ('group', 'user_email', 'status', 'date',)
     list_filter = ('group', 'user', 'status',)
     search_fields = ('user__first_name', 'user__last_name', 'user__email', 'group__name')
+    raw_id_fields = ('user',)
 
 admin.site.register(CosinnusPortalMembership, PortalMembershipAdmin)
 
