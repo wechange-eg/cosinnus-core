268ec74ba569c3f457398a8538ea95ebca3ac65a
Sascha Narr <saschanarr@googlemail.com>
2015-07-29 12:44:35 +0200

    Add raw_id_field for user group membership

--- cosinnus/admin.py
+++ cosinnus/admin.py
@@ -48,6 +48,7 @@ class MembershipAdmin(admin.ModelAdmin):
     list_display = ('group', 'user_email', 'status', 'date',)
     list_filter = ('group', 'user', 'status',)
     search_fields = ('user__first_name', 'user__last_name', 'user__email', 'group__name')
+    raw_id_fields = ('user',)
     
 admin.site.register(CosinnusGroupMembership, MembershipAdmin)
 
