67970883184e43ce3db1faa4b0beb02b1990d8be
Sascha Narr <saschanarr@googlemail.com>
2015-08-03 15:06:43 +0200

    Fix write view allowing non-logged in users

--- cosinnus/core/decorators/views.py
+++ cosinnus/core/decorators/views.py
@@ -294,9 +294,8 @@ def require_write_access(group_url_kwarg='group', group_attr='group'):
             except (AttributeError, TypeError):
                 pass
             
-            obj_public = requested_object and getattr(requested_object, 'media_tag', None) \
-                and requested_object.media_tag.visibility == BaseTagObject.VISIBILITY_ALL
-            if not (obj_public or user.is_authenticated()):
+            # objects can never be written by non-logged in members
+            if not user.is_authenticated():
                 return redirect_to_not_logged_in(request, view=self)
             
             if requested_object:
