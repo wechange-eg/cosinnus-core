85d6b8ac3010b5698d6c87fe8ff89a838679c14a
Sascha Narr <saschanarr@googlemail.com>
2015-10-14 16:44:52 +0200

    cleanup imports

--- cosinnus/views/microsite.py
+++ cosinnus/views/microsite.py
@@ -1,23 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.utils.translation import ugettext_lazy as _
 from django.views.generic import TemplateView
 
-from cosinnus.views.mixins.group import RequireWriteMixin, DipatchGroupURLMixin,\
-    GroupObjectCountMixin
-from cosinnus.models.widget import WidgetConfig
+from cosinnus.views.mixins.group import DipatchGroupURLMixin, GroupObjectCountMixin
 from cosinnus.conf import settings
-from cosinnus.models.group import CosinnusGroup
 from django.http.response import Http404
-from cosinnus.models.cms import CosinnusMicropage
-from cosinnus.core.registries import widget_registry
-from cosinnus.core.decorators.views import redirect_to_403,\
-    get_group_for_request
-from cosinnus.utils.permissions import check_object_write_access
-from cosinnus.core.registries.attached_objects import attached_object_registry
-from cosinnus.utils.functions import get_cosinnus_app_from_class
-from cosinnus.views.widget import DashboardWidgetMixin
 
 
 class GroupMicrositeView(DipatchGroupURLMixin, GroupObjectCountMixin, TemplateView):
