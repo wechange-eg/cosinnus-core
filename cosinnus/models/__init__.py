e9159f1a9193d4e9e5d83cce7f8e74ea62a8db85
Sascha Narr <saschanarr@googlemail.com>
2015-10-29 14:58:38 +0100

    Import dependency cleanup

--- cosinnus/models/__init__.py
+++ cosinnus/models/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
+# group must be imported first
+from cosinnus.models.group import *  # noqa
+from cosinnus.models.tagged import *  # noqa
 from cosinnus.models.cms import *  # noqa
 from cosinnus.models.feedback import *  # noqa
-from cosinnus.models.group import *  # noqa
 from cosinnus.models.profile import *  # noqa
-from cosinnus.models.tagged import *  # noqa
 from cosinnus.models.widget import *  # noqa
