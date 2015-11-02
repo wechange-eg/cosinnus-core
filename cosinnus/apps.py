020e467d18156616dd9b6197713c83b957262cdd
Sascha Narr <saschanarr@googlemail.com>
2015-11-02 17:12:30 +0100

    Django 1.8 app loading

--- cosinnus/apps.py
+++ cosinnus/apps.py
@@ -5,7 +5,7 @@ from django.apps import AppConfig
 class CosinnusAppConfig(AppConfig):
 
     name = 'cosinnus'
-    verbose_name = 'My App'
+    verbose_name = 'Cosinnus Core'
 
     def ready(self):
-        print ">>>>>>>>>>>>>> cosinnus app received ready()"
+        pass
