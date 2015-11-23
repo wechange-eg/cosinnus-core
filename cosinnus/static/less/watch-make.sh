806b86b1d40dc9dd92c8a54a9b1a445b3884fb1b
Thomas Schmidt <ts@sinnwerkstatt.com>
2015-11-23 14:32:50 +0100

    Lieber auf Dev und Production 100% das selbe CSS. Und beim Deploy kein lessc mehr brauchen. Dafür watch-make.sh laufen lassen. Abhängigkeiten: lessc, inotifywait (reverted from commit 79b9514a6ed6e552b2c08390decd482aabf5cda3)

--- cosinnus/static/less/watch-make.sh
+++ cosinnus/static/less/watch-make.sh
@@ -1,6 +1,1 @@
-# move to this script's path
-cd "${0%/*}"
-lessc cosinnus.less > ../css/cosinnus.css
-echo cosinnus.css created.
-echo Now watching Cosinnus LESS files...
 while inotifywait -e close_write,moved_to,create .; do lessc cosinnus.less > ../css/cosinnus.css; done
