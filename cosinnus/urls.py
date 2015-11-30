c334128e2643252efab76344dbde525d63fd7f03
Sascha Narr <saschanarr@googlemail.com>
2015-11-30 15:26:46 +0100

    Added housekeeping cache test

--- cosinnus/urls.py
+++ cosinnus/urls.py
@@ -41,7 +41,8 @@ urlpatterns = patterns('cosinnus.views',
     url(r'^housekeeping/deletespamusers/$', 'housekeeping.delete_spam_users', name='housekeeping_delete_spam_users'),
     url(r'^housekeeping/movegroupcontent/(?P<fromgroup>[^/]+)/(?P<togroup>[^/]+)/$', 'housekeeping.move_group_content', name='housekeeping_move_group_content'),
     url(r'^housekeeping/recreategroupwidgets/$', 'housekeeping.recreate_all_group_widgets', name='housekeeping_recreate_all_group_widgets'),
-    
+    url(r'^housekeeping/setcache/(?P<content>[^/]+)/$', 'housekeeping.setcache', name='housekeeping-setcache'),
+    url(r'^housekeeping/getcache$', 'housekeeping.getcache', name='housekeeping-getcache'),
 
     url(r'^select2/', include('cosinnus.urls_select2', namespace='select2')),
 )
