15c44546097728b4dd5f9034cb3beffb105ef359
Sascha Narr <saschanarr@googlemail.com>
2016-01-07 13:33:34 +0100

    Moved utils and hooks into core. Refactored model signal/hooks into hooks.py

--- cosinnus/models/__init__.py
+++ cosinnus/models/__init__.py
@@ -7,3 +7,4 @@ from cosinnus.models.cms import *  # noqa
 from cosinnus.models.feedback import *  # noqa
 from cosinnus.models.profile import *  # noqa
 from cosinnus.models.widget import *  # noqa
+
