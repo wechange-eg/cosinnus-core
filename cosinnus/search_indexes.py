656ac2e2a04fa3d170bdac1225c4f00afa96c385
Sascha Narr <saschanarr@googlemail.com>
2016-01-07 13:48:00 +0100

    Search indexes into core

--- cosinnus/search_indexes.py
+++ cosinnus/search_indexes.py
@@ -27,3 +27,24 @@ class GroupIndex(TagObjectSearchIndex, indexes.Indexable):
         qs = qs.filter(is_active=True)
         qs = qs.select_related('media_tag').all()
         return qs
+
+
+class TagObjectIndex(indexes.SearchIndex):
+    mt_location = indexes.CharField(model_attr='media_tag__location', null=True)
+    mt_location_lat = indexes.FloatField(model_attr='media_tag__location_lat', null=True)
+    mt_location_lon = indexes.FloatField(model_attr='media_tag__location_lon', null=True)
+    mt_place = indexes.CharField(model_attr='media_tag__place', null=True)
+    mt_valid_start = indexes.DateTimeField(model_attr='media_tag__valid_start', null=True)
+    mt_valid_end = indexes.DateTimeField(model_attr='media_tag__valid_end', null=True)
+    mt_approach = indexes.CharField(model_attr='media_tag__approach', null=True)
+    mt_topics = indexes.MultiValueField(model_attr='media_tag__topics', null=True)
+    mt_persons = indexes.MultiValueField(null=True)
+    mt_likes = indexes.IntegerField(model_attr='media_tag__likes', null=True)
+    mt_visibility = indexes.BooleanField(model_attr='media_tag__visibility', null=True)
+
+    def prepare_mt_persons(self, obj):
+        if obj.media_tag:
+            return prepare_users(obj.media_tag.persons.all())
+
+
+from cosinnus.utils.search import prepare_users
