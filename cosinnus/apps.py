9ef1e81d7e9a1beeda94299fae0c734fa626811b
Sascha Narr <saschanarr@googlemail.com>
2016-01-21 18:58:53 +0100

    Moved hook registers to hooks.py

--- cosinnus/apps.py
+++ cosinnus/apps.py
@@ -8,14 +8,8 @@ class CosinnusAppConfig(AppConfig):
     verbose_name = 'Cosinnus Core'
 
     def ready(self):
+        # connect all signal listeners
         from cosinnus.models.hooks import *  # noqa
-        
-        from cosinnus.models.tagged import ensure_container
-        from cosinnus.core.registries.group_models import group_model_registry
-        post_save.connect(ensure_container, sender=CosinnusGroup)
-        for url_key in group_model_registry:
-            group_model = group_model_registry.get(url_key)
-            post_save.connect(ensure_container, sender=group_model)
             
         from cosinnus.models.group import replace_swapped_group_model
         replace_swapped_group_model()
