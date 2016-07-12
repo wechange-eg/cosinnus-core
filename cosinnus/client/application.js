6ea3560ab9b06320d26f6291463ae6557facd45b
Sascha Narr <saschanarr@googlemail.com>
2016-07-12 14:52:00 +0200

    debug console logs for Map JS

--- cosinnus/client/application.js
+++ cosinnus/client/application.js
@@ -31,6 +31,7 @@ module.exports = function Application () {
         self.mediator.settings = window.settings || {};
         self.mediator.subscribe('navigate:router', function (event, url) {
             if (url) {
+                console.log('navigating url ap1' + url)
                 self.router.navigate(url, {
                     trigger: false
                 });
