0b798a6d4c1dcc958f42080429e041fdf799a7e2
Sascha Narr <saschanarr@googlemail.com>
2016-09-05 17:31:22 +0200

    HTML mails: Adjusted rwif helper to print out the named if-variable into the raw if-block

--- cosinnus/html_emails/src/helpers/rwif.js
+++ cosinnus/html_emails/src/helpers/rwif.js
@@ -2,7 +2,8 @@ module.exports = function(booleanish, options) {
     var content = options.fn(this);
 
     if (options.data.root.raw) {
-        return '{% if ' + booleanish + '%}' + content + '{% endif %}';
+        var raw_if = options.hash['raw_if'];
+        return '{% if ' + raw_if + ' %}' + content + '{% endif %}';
     } else if (booleanish) {
         return content;
     }
