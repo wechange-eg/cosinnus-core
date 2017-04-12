2b0287df439edd53853a1384c732e40a96e70745
Sascha Narr <saschanarr@googlemail.com>
2017-04-12 17:35:56 +0200

    Added zip creation function with StringIO to files utils

--- cosinnus/utils/files.py
+++ cosinnus/utils/files.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 import hashlib
+import os
+import StringIO
 import tempfile
 import zipfile
 
@@ -75,3 +77,30 @@ def create_zip_file(files):
                 else:
                     missing.append(f[1])
     return tf, missing
+
+
+def create_zip_from_files(file_list):
+    """ Will create an in-memory (StringIO) Zip-File from files on the disk. 
+        @param file_list: A list of string tuples: [(local_file_path, relative_zip_path)]
+            Example: [('/tmp/file1.txt', 'file1.txt'), ('/tmp/sub/file2.txt', 'sub/file2.txt')]
+        @return: A StringIO instance containing the zip in memory """
+        
+    # Open StringIO to grab in-memory ZIP contents
+    stringio = StringIO.StringIO()
+    # The zip compressor
+    zip_file = zipfile.ZipFile(stringio, "w", zipfile.ZIP_DEFLATED)
+    # add files to zip with custom path
+    for file_path, zip_path in file_list:
+        if path.exists(file_path):
+            zip_file.write(file_path, zip_path)
+    # Must close zip for all contents to be written
+    zip_file.close()
+    
+    return stringio
+
+def append_string_to_filename(file_path, string_to_append):
+    """ Appends a string to *the base file name*, before the file extension, of a given file with path """
+    dir_name, file_name = os.path.split(file_path)
+    file_root, file_ext = os.path.splitext(file_name)
+    return os.path.join(dir_name, "%s_%s%s" % (file_root, string_to_append, file_ext))
+                
