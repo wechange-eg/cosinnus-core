61dfb0221338f47327fca43a000c860c958e0c2e
Sascha Narr <saschanarr@googlemail.com>
2017-04-20 18:12:08 +0200

    Added create_user() util function and create user in SSO flow

--- cosinnus/views/sso.py
+++ cosinnus/views/sso.py
@@ -13,8 +13,18 @@ from django.views.generic.base import TemplateView
 from django.core.urlresolvers import reverse
 
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import get_user_model
+from annoying.functions import get_object_or_None
+from cosinnus.models.profile import get_user_profile_model
+from cosinnus.utils.user import create_user
+from django.utils.crypto import get_random_string
+from django.contrib.auth import login as django_login
 
 logger = logging.getLogger('cosinnus')
+
+SSO_USERPROFILE_FIELD_ID = 'sso_id'
+SSO_USERPROFILE_FIELD_OAUTH_TOKEN = 'sso_token'
+SSO_USERPROFILE_FIELD_OAUTH_SECRET = 'sso_token_secret'
 
 
 def login(request):
@@ -49,14 +59,36 @@ def callback(request):
     
     if not all([bool(prop in user_info) for prop in ['username', 'email', 'id']]):
         logger.error('Exception during SSO login, not all expected properties in returned user info JSON!', extra={'user_info': user_info})
-        messages.error(request, _('Sorry, we could not connect your user account, because we could not retrieve important user account infos!'))
+        messages.error(request, _('Sorry, we could not connect your user account, because we could not retrieve important user account infos. Please contact a system administrator!'))
         return redirect('cosinnus:sso-error')
     
-    # TODO: if the user doesn't exist yet, create a user acount and portal membership for him
+    # match user over ID, never email! this could be used to take over other user accounts if the SSO server does not enforce email validation
+    # because of this, we actually may end up with non-unique emails in cosinnus, but since regular authentication is disabled, 
+    # this should not cause problems
+    user = get_object_or_None(get_user_profile_model(), settings_containts={SSO_USERPROFILE_FIELD_ID: user_info['id']})
     
-    # TODO: if the user exists, log him in and save/update his profile info and oauth_token and oauth_secret in his settings
+    # if the user doesn't exist yet, create a user acount and portal membership for him
+    if not user:
+        # we create the user with a random email to get around cosinnus' unique-email validation,
+        # because we cannot be sure if the SSO server has unique emails we will replace the email here later on
+        user = create_user(
+            '%@none.com' % get_random_string(),
+            first_name=user_info.get('first_name', user_info.get('username')),
+            last_name=user_info.get('last_name', None)
+        )
+        if not user:
+            logger.error('Exception during SSO login, User could not be created!', extra={'user_info': user_info})
+            messages.error(request, _('Sorry, we could not connect your user account because of an internal error. Please contact a system administrator!') + '(1)')
+            return redirect('cosinnus:sso-error')
+        user.cosinnus_profile.settings[SSO_USERPROFILE_FIELD_ID] = user_info['id']
+        user.cosinnus_profile.save()
     
-    # TODO: log user in
+    # TODO: save/update his profile info and email and oauth_token and oauth_secret in his settings
+    
+    # set user avatar
+    
+    # log user in
+    django_login(request, user)
     
     return redirect(settings.COSINNUS_SSO_ALREADY_LOGGED_IN_REDIRECT_URL)
     
