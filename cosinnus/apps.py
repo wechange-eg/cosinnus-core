49f25490bdb48f90221b1968f78046a33c925398
Sascha Narr <saschanarr@googlemail.com>
2017-04-20 18:11:10 +0200

    IMPORTANT FIX: fixed missing hooks being connected

--- cosinnus/apps.py
+++ cosinnus/apps.py
@@ -10,3 +10,4 @@ class CosinnusAppConfig(AppConfig):
     def ready(self):
         from cosinnus.models.group import replace_swapped_group_model
         replace_swapped_group_model()
+        from cosinnus.models.hooks import *
