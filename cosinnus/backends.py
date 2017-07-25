7180a5cbfbce74d7e819ceaa7dbc6da63ad3e894
Sascha Narr <saschanarr@googlemail.com>
2017-07-25 19:11:59 +0200

    Added RobustElasticSearchEngine that doesn't crash on index errors.

--- cosinnus/backends.py
+++ cosinnus/backends.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 from django.core.mail.backends.smtp import EmailBackend
+from django.utils.decorators import method_decorator
 
 from cosinnus.utils.user import get_user_by_email_safe
 from cosinnus.conf import settings
@@ -11,6 +12,14 @@ from cosinnus.conf import settings
 import dkim
 import smtplib
 from django.core.mail.message import sanitize_address
+
+from haystack.backends.elasticsearch_backend import ElasticsearchSearchBackend, ElasticsearchSearchEngine
+from urllib3.exceptions import ProtocolError, ConnectionError
+from elasticsearch.exceptions import TransportError
+
+import logging 
+from django.utils.encoding import force_text
+logger = logging.getLogger('cosinnus')
 
 
 USER_MODEL = get_user_model()
@@ -61,4 +70,32 @@ class DKIMEmailBackend(EmailBackend):
             return False
         return True
     
-    
+
+class RobustElasticSearchBackend(ElasticsearchSearchBackend):
+    """A robust backend that doesn't crash when no connection is available"""
+
+    def mute_error(f):
+        def error_wrapper(self, *args, **kwargs):
+            try:
+                return f(self, *args, **kwargs)
+            except (TransportError, ProtocolError, ConnectionError) as e:
+                logger.exception('Could not connect to the ElasticSearch backend for indexing! The search function will not work and saving objects on the site will be terribly slow! Exception in extra.', extra={'exception': force_text(e)})
+        return error_wrapper
+
+    def __init__(self, connectionalias, **options):
+        super(RobustElasticSearchBackend, self).__init__(connectionalias, **options)
+
+    @mute_error
+    def update(self, indexer, iterable, commit=True):
+        super(RobustElasticSearchBackend, self).update(indexer, iterable, commit)
+
+    @mute_error
+    def remove(self, obj, commit=True):
+        super(RobustElasticSearchBackend, self).remove(obj, commit)
+
+    @mute_error
+    def clear(self, models=[], commit=True):
+        super(RobustElasticSearchBackend, self).clear(models, commit)
+
+class RobustElasticSearchEngine(ElasticsearchSearchEngine):
+    backend = RobustElasticSearchBackend
