db24e0eaa6ec1157df1bce0b39d213f69e6872c0
Sascha Narr <saschanarr@googlemail.com>
2018-10-16 15:53:15 +0200

    Haystack adjustments for changed signal processor path

--- cosinnus/models/mixins/indexes.py
+++ cosinnus/models/mixins/indexes.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 
 from builtins import object
 import logging
-import haystack
+from haystack.signals import BaseSignalProcessor
+from haystack import connection_router, connections
 
 logger = logging.getLogger('cosinnus')
 
@@ -11,12 +12,14 @@ logger = logging.getLogger('cosinnus')
 class IndexingUtilsMixin(object):
     """ Adds update/remove index util functions to a model """
     
+    signal_processor = BaseSignalProcessor(connections, connection_router)
+    
     def update_index(self):
         """ Updates self in the proper search index depending on model """
-        haystack.signal_processor.handle_save(self.__class__, self)
+        self.signal_processor.handle_save(self.__class__, self)
         
     def remove_index(self):
         """ Removes self from the proper search index depending on model """
-        haystack.signal_processor.handle_delete(self.__class__, self)
+        self.signal_processor.handle_delete(self.__class__, self)
         
     
