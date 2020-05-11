84b73af41fec4bf9ddca7c1f59ad99484e0727e6
sascha <sn@sinnwerkstatt.com>
2020-05-11 13:56:04 +0200

    Adjust collect-po.js to also incorporate locale_extra PO files

--- cosinnus/client/lib/collect-po.js
+++ cosinnus/client/lib/collect-po.js
@@ -10,19 +10,36 @@ module.exports = {
         var podicts = {};
         _.each(languages, function(lang){
             try {
-                var po_contents = fs.readFileSync('./cosinnus/locale/' + lang + '/LC_MESSAGES/django.po', 'utf-8').toString();
-                var po = PO.parse(po_contents);
+                // we try to read the cosinnus-core po file and the main server project po file 
+                // (which is copied to the cosinnus-core directory during deploy)
+                var paths = [
+                    './cosinnus/locale/' + lang + '/LC_MESSAGES/django.po',
+                    './cosinnus/locale_extra/' + lang + '/LC_MESSAGES/django.po',
+                ];
                 var langdict = {};
-                _.each(po.items, function(item){
-                    // check for those translation items that contain "__INCLUDE_JS_PO__" as comment
-                    if (_.some(item.extractedComments, function(comment){return comment.indexOf("__INCLUDE_JS_PO__") >= 0})) {
-                        if (!item.obsolete && !item.flags.fuzzy) {
-                            langdict[item.msgid] = item.msgstr[0];
-                        }
+                _.each(paths, function(path){
+                    var po_contents = null;
+                    try {
+                        po_contents = fs.readFileSync(path, 'utf-8').toString();
+                    } catch (e) {
+                        console.log('Could not open a translation PO file for a language for JS generation. If this language is not supported in the current cosinnus project, you can safely ignore this message.\nThe error was:')
+                        console.log(e)
+                    }
+                    if (po_contents) {
+                        var po = PO.parse(po_contents);
+                        _.each(po.items, function(item){
+                            // check for those translation items that contain "__INCLUDE_JS_PO__" as comment
+                            if (_.some(item.extractedComments, function(comment){return comment.indexOf("__INCLUDE_JS_PO__") >= 0})) {
+                                if (!item.obsolete && !item.flags.fuzzy) {
+                                    langdict[item.msgid] = item.msgstr[0];
+                                }
+                            }
+                        });
                     }
                 });
                 podicts[lang] = langdict;
             } catch (e) {
+                console.log(e)
             }
         });
         return podicts;
