f3cfea32586a878e1291e7a7c6975f06917b6750
Magdalena Noffke <magdalena.noffke@googlemail.com>
2020-07-14 16:51:31 +0200

    Add fist provider version

---
+++ cosinnus_oauth_client/provider.py
@@ -0,0 +1,30 @@
+from allauth.socialaccount.providers.base import ProviderAccount
+from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
+
+from django.conf import settings
+
+
+class CosinnusOauthClientAccount(ProviderAccount):
+    pass
+
+class CosinnusOauthClientProvider(OAuth2Provider):
+
+    id = 'wechange'
+    name = 'Cosinnus OAuth Provider'
+    account_class = CosinnusOauthClientAccount
+
+    def extract_uid(self, data):
+        return str(data['id'])
+
+    def extract_common_fields(self, data):
+        return dict(username=data['username'],
+                    email=data['email'],
+                    first_name=data['first_name'],
+                    last_name=data['last_name'],)
+
+    def get_default_scope(self):
+        scope = ['read']
+        return scope
+
+
+provider_classes = [CosinnusOauthClientProvider]
---
+++ cosinnus_oauth_client/views.py
@@ -0,0 +1,43 @@
+from allauth.socialaccount.providers.oauth2.views import (OAuth2Adapter,
+                                                          OAuth2CallbackView,
+                                                          OAuth2LoginView)
+from allauth.account.adapter import DefaultAccountAdapter
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+
+from cosinnus.utils.urls import redirect_with_next
+from django.urls import reverse
+
+from django.conf import settings
+from cosinnus.models.profile import get_user_profile_model
+
+import requests
+
+from .provider import CosinnusOauthClientProvider
+
+
+class CosinusAllauthRedirectAdapter(DefaultAccountAdapter):
+
+    def get_login_redirect_url(self, request):
+        return redirect_with_next(reverse('cosinnus:user-dashboard'), self.request)
+
+
+class CosinnusOauthClientAdapter(OAuth2Adapter):
+    provider_id = CosinnusOauthClientProvider.id
+    access_token_url = '{}/o/token/'.format(settings.COSINNUS_OAUTH_SERVER_BASEURL)
+    user_url = '{}/o/user/'.format(settings.COSINNUS_OAUTH_SERVER_BASEURL)
+    authorize_url = '{}/o/authorize/'.format(settings.COSINNUS_OAUTH_SERVER_BASEURL)
+
+
+    def complete_login(self, request, app, token, **kwargs):
+        url = self.user_url
+        headers = {'Authorization': 'Bearer {0}'.format(token.token)}
+        resp = requests.get(url, headers=headers)
+        resp.raise_for_status()
+        extra_data = resp.json()
+        socialaccount = self.get_provider().sociallogin_from_response(request,
+                                                                      extra_data)
+        return socialaccount
+
+
+oauth2_login = OAuth2LoginView.adapter_view(CosinnusOauthClientAdapter)
+oauth2_callback = OAuth2CallbackView.adapter_view(CosinnusOauthClientAdapter)
