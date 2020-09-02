3f3edb25c210f9f4057b28dc7f0138e88fda9a8b
Tobias Dietze <tobias.dietze@opox.org>
2020-09-02 15:35:56 +0200

    added BBB api wrapper for Django
    * added django-bigbluebutton library
    * added django-bigbluebutton dependencies
    * updated BBBRoom model field definitions
    * added model migrations

--- cosinnus/models/bbb_room.py
+++ cosinnus/models/bbb_room.py
@@ -1,7 +1,15 @@
+import string
+import random
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 from django.contrib.postgres.fields import JSONField
+
+
+def random_password(length=5):
+    """ generates a random moderator password for a BBBRoom  with lowercase ASCII characters """
+    return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
 
 
 class BBBRoom(models.Model):
@@ -26,10 +34,16 @@ class BBBRoom(models.Model):
     presenter = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name="presenter",
                                   help_text=_("this user will enter the BBB presenter mode for this conversation"))
     members = models.ManyToManyField(User, related_name="members")
-    member_token = JSONField(default={}, verbose_name="member token mapping")
+    member_token = JSONField(default=dict, verbose_name="member token mapping")
     moderators = models.ManyToManyField(User)
     token = models.CharField(max_length=200, null=True, blank=True)
     name = models.CharField(max_length=160, null=True, blank=True, verbose_name=_("room name or title"))
+    moderator_password = models.CharField(max_length=30, default=random_password,
+                                          help_text=_("the password set to enter the room as a moderator"))
+    attendee_password = models.CharField(max_length=30, default='', null=True, blank=True,
+                                         help_text=_("the password set to enter the room as a attendee"))
+    welcome_message = models.CharField(max_length=300, null=True, blank=True, verbose_name=_("the rooms welcome message"),
+                                       help_text=_("the welcome message, that is displayed to attendees"))
 
     def __str__(self):
         return str(self.token)
