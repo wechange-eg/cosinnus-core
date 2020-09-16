8a3f7e8cc190e9269adc2d865916b521e435191c
sascha <sn@sinnwerkstatt.com>
2020-09-16 11:22:30 +0200

    Add init.py and apps.py for cosinnus_conference

---
+++ cosinnus_conference/apps.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+
+class CosinnusConferenceAppConfig(AppConfig):
+
+    name = 'cosinnus_conference'
+    verbose_name = 'Cosinnus Conference'
+
+    def ready(self):
+        pass
