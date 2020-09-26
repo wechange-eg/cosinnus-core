0e6047fb717ac3bf6532f06fbe830edf8a6905b8
Simon Liedtke <simon.liedtke@gmail.com>
2020-09-26 10:43:06 +0200

    Fixed room link

--- cosinnus_conference/frontend/src/views/components/CoffeeTable/index.tsx
+++ cosinnus_conference/frontend/src/views/components/CoffeeTable/index.tsx
@@ -40,7 +40,7 @@ export function CoffeeTable(props: CoffeeTableProps & RouteComponentProps) {
     <Card className={classes.card}>
       <CardActionArea
         onClick={() => {
-          if (participantsCount === undefined) return
+          if (participantsCount === 0) return
           const url = event.getUrl()
           if (url) window.location.href = url
         }}
