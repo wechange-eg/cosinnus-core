a5defb178d1e8cd58f75c6d0cf8a553a3e111ff2
Simon Liedtke <simon.liedtke@gmail.com>
2020-10-19 21:44:25 +0200

    API changes for Dina

--- cosinnus/api/views/__init__.py
+++ cosinnus/api/views/__init__.py
@@ -2,6 +2,7 @@ import json
 
 from django.contrib.auth import get_user_model
 from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.db.models import Q
 from django.http import HttpResponse
 from django.template import Context
 from django.template.loader import render_to_string
@@ -45,15 +46,20 @@ class PublicTaggableObjectFilterMixin(object):
 
 class CosinnusFilterQuerySetMixin:
     FILTER_CONDITION_MAP = {
+        'avatar': {
+            'true': [~Q(avatar="")],
+            'false': [Q(avatar="")],
+        }
     }
     FILTER_KEY_MAP = {
-        'tags': 'media_tag__tags__name'
+        'tags': 'media_tag__tags__name',
     }
     FILTER_VALUE_MAP = {
         'false': False,
         'true': True
     }
     FILTER_DEFAULT_ORDER = ['-created', ]
+    MANAGED_TAGS_KEY = 'managed_tags'
 
     def get_queryset(self):
         """
@@ -65,6 +71,12 @@ class CosinnusFilterQuerySetMixin:
         query_params = self.request.query_params.copy()
         order_by = query_params.pop('order_by', self.FILTER_DEFAULT_ORDER)
         queryset = queryset.order_by(*order_by)
+        # Managed tag filters
+        if self.MANAGED_TAGS_KEY in query_params:
+            managed_tags = query_params.getlist(self.MANAGED_TAGS_KEY)
+            queryset = queryset.filter(managed_tag_assignments__managed_tag__slug__in=managed_tags,
+                                       managed_tag_assignments__approved=True)
+            query_params.pop(self.MANAGED_TAGS_KEY)
         # Overwrite ugly but commonly used filters
         for key, value in list(query_params.items()):
             if key in (self.pagination_class.limit_query_param,
@@ -73,14 +85,7 @@ class CosinnusFilterQuerySetMixin:
             if key in self.FILTER_CONDITION_MAP:
                 VALUE_MAP = self.FILTER_CONDITION_MAP.get(key)
                 if value in VALUE_MAP:
-                    (key, value), *rest = VALUE_MAP.get(value).items()
-            key = self.FILTER_KEY_MAP.get(key, key)
-            value = self.FILTER_VALUE_MAP.get(value, value)
-            if value is not None:
-                if key.startswith('exclude_'):
-                    queryset = queryset.exclude(**{key[8:]: value})
-                else:
-                    queryset = queryset.filter(**{key: value})
+                    queryset = queryset.filter(*VALUE_MAP.get(value))
         return queryset
 
 
