eb8d9f156e09120b8634873e42c6e4669dc4fb0e
Tobias Dietze <tobias.dietze@opox.org>
2020-11-05 14:07:03 +0100

    fixed test cases for BSISafePasswordValidator
    * all password conventions are tested by automated tests

--- cosinnus/tests/boell/test_validators.py
+++ cosinnus/tests/boell/test_validators.py
@@ -1,11 +1,18 @@
+import json
+
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import get_default_password_validators
 from django.contrib.auth.forms import SetPasswordForm
 from django.conf import settings
-from django.test import TestCase, RequestFactory
+from django.test import TestCase, RequestFactory, override_settings
 
 
+@override_settings(AUTH_PASSWORD_VALIDATORS=[{'NAME': 'cosinnus.utils.validators.BSISafePasswordValidator',}])
 class ValidatorTestCase(TestCase):
+    """ Testcase for the BSISafePasswordValidator checks the following cases
+
+    """
+
     def setUp(self):
         # skipping test if the validators to test are not installed
         if not get_default_password_validators():
@@ -14,6 +21,15 @@ class ValidatorTestCase(TestCase):
 
         self.user = User(username="testuser", email="123@123.com")
         self.user.save()
+
+    def get_error_message_code(self, errors, field_name="new_password2", code_field="code"):
+        message_list = json.loads(errors.as_json())[field_name]
+        result_codes = []
+
+        for entry in message_list:
+            result_codes.append(entry.get(code_field, ""))
+
+        return result_codes
 
     def test_correct_password(self):
         form = SetPasswordForm(
@@ -34,19 +50,11 @@ class ValidatorTestCase(TestCase):
                 "new_password2": "OtherPassword!2"
             }
         )
-        print(form.errors)
-        self.assertFalse(form.is_valid())
 
-    def test_mismatching_passwords2(self):
-        form = SetPasswordForm(
-            user=self.user,
-            data={
-                "new_password1": "OnePassword!2",
-                "new_password2": "OtherPassword!2"
-            }
+        self.assertFalse(form.is_valid())
+        self.assertIn(
+            'password_mismatch', self.get_error_message_code(form.errors)
         )
-        print(form.errors)
-        self.assertTrue(form.is_valid())
 
     def test_missing_special_character(self):
         form = SetPasswordForm(
@@ -56,9 +64,10 @@ class ValidatorTestCase(TestCase):
                 "new_password2": "1pasSword"
             }
         )
-        print(form.errors)
-
         self.assertFalse(form.is_valid())
+        self.assertIn(
+            'password_no_special_character', self.get_error_message_code(form.errors)
+        )
 
     def test_missing_capital_letter(self):
         form = SetPasswordForm(
@@ -68,9 +77,11 @@ class ValidatorTestCase(TestCase):
                 "new_password2": "1password!"
             }
         )
-        print(form.errors)
 
         self.assertFalse(form.is_valid())
+        self.assertIn(
+            'password_no_uppercase_character', self.get_error_message_code(form.errors)
+        )
 
     def missing_small_letter(self):
         form = SetPasswordForm(
@@ -80,5 +91,22 @@ class ValidatorTestCase(TestCase):
                 "new_password2": "1PASSWORD!"
             }
         )
-        print(form.errors)
+
         self.assertFalse(form.is_valid())
+        self.assertIn(
+            'password_no_lowercase_character', self.get_error_message_code(form.errors)
+        )
+
+    def missing_digit(self):
+        form = SetPasswordForm(
+            user=self.user,
+            data={
+                "new_password1": "password!",
+                "new_password2": "password!"
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertIn(
+            'password_no_digit', self.get_error_message_code(form.errors)
+        )
