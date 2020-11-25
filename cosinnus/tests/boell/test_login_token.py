3d24bd06b38a46f507d29c3aeb60a133b14dee94
Tobias Dietze <tobias.dietze@opox.org>
2020-11-25 07:15:47 +0100

    added test cases for set password middleware

--- cosinnus/tests/boell/test_login_token.py
+++ cosinnus/tests/boell/test_login_token.py
@@ -1,31 +1,69 @@
 import json
 
 from django.contrib.auth.models import User
-from django.contrib.auth.password_validation import get_default_password_validators
-from django.contrib.auth.forms import SetPasswordForm
-from django.conf import settings
-from django.test import TestCase, RequestFactory, override_settings
-from django.core.mail import send_mail
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import reverse
+from django.test import TestCase, Client
 from cosinnus.utils.user import create_base_user
+from cosinnus.models.profile import PROFILE_SETTING_PASSWORD_NOT_SET
 
 
 class EmailTestCase(TestCase):
     def setUp(self):
-        pass
+        self.user = create_base_user("mail1@example.com", no_generated_password=True)
+        self.token = self.user.cosinnus_profile.settings[PROFILE_SETTING_PASSWORD_NOT_SET]
 
-    @override_settings(EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend')
-    def test_manual_user_creation(self):
-        user = create_base_user("tobias.dietze@gmail.com", username="test", no_generated_password=True)
+        self.other_user = User(username="mail2@example.com", email="testmail@example.com")
+        self.other_user.set_password("password")
+        self.other_user.save()
 
-        self.assertEqual(user.username, "test")
-        self.assertEqual(user.password, None)
+    def test_token_removed_from_profile(self):
+        client = Client()
 
-    # @override_settings(EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend')
-    # def test_send_mail(self):
-    #     send_mail(
-    #         'Subject here',
-    #         'Here is the message.',
-    #         'recruiting@opox.org',
-    #         ['dietze.tobias@gmail.com'],
-    #         fail_silently=False,
-    #     )
+        new_user = create_base_user("mail3@example.com", no_generated_password=True)
+        token = new_user.cosinnus_profile.settings[PROFILE_SETTING_PASSWORD_NOT_SET]
+        self.assertIn(PROFILE_SETTING_PASSWORD_NOT_SET, new_user.cosinnus_profile.settings)
+
+        # post new password to the view method
+        data = {'new_password1': "_chahgheiC3ye", "new_password2": "_chahgheiC3ye"}
+
+        response = client.post(reverse('password_set_initial', kwargs={'token': token}), data=data)
+
+        user = User.objects.get(email="mail3@example.com")
+        self.assertNotEqual(user.password, "")
+        self.assertEqual(response.status_code, 200)
+
+    def test_set_password_view_available(self):
+        client = Client()
+
+        response = client.get(reverse('password_set_initial', kwargs={'token': self.token}))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(PROFILE_SETTING_PASSWORD_NOT_SET, response.client.cookies)
+
+    def test_password_view_with_logged_in_user(self):
+        client = Client()
+
+        # login the other user to ensure a proper session setup
+        client.force_login(self.other_user)
+
+        with self.assertRaises(PermissionDenied):
+            response = client.get(reverse('password_set_initial', kwargs={'token': self.token}))
+
+    def test_redirected_by_middleware(self):
+        client = Client()
+
+        # first request, to set the cookie
+        response = client.get(reverse('password_set_initial', kwargs={'token': self.token}))
+
+        # try accessing an other view
+        response = client.get(reverse('cosinnus:user-dashboard'))
+        self.assertEqual(response.url, reverse('password_set_initial'))
+
+    def test_double_access(self):
+        client = Client()
+        self.user.set_password('password')
+        self.user.save()
+
+        # first request, to set the cookie
+        response = client.get(reverse('password_set_initial', kwargs={'token': self.token}))
+        self.assertEqual(response.status_code, 404)
