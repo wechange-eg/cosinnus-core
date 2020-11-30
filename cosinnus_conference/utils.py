66a4ab2b4ecd1013d90b51b5258ebd6b9b7f42ab
Simon Liedtke <simon.liedtke@gmail.com>
2020-11-30 23:07:16 +0100

    Added conference reminders

--- cosinnus_conference/utils.py
+++ cosinnus_conference/utils.py
@@ -1,10 +1,11 @@
 from datetime import datetime
+import re
 
-from django.template import Context, Template
+from django.template import Template, Context
 from django.template.loader import get_template
 from django.utils.encoding import force_text
 
-from cosinnus.core.mail import send_html_mail_threaded
+from cosinnus.core.mail import send_html_mail_threaded, get_common_mail_context
 
 
 def get_initial_template(field_name):
@@ -13,29 +14,26 @@ def get_initial_template(field_name):
     """
     content_template_name = 'cosinnus/mail/conference_reminder_%s.txt'
     template = get_template(content_template_name % field_name)
-    # FIXME: Remove translation feature
-    template = template.template.source.replace('{% load i18n %}', '')
-    template = template.replace('{% blocktrans %}', '').replace('{% endblocktrans %}', '')
-    return template
+    return template.render({})  # render to trigger translate
 
 
 def send_conference_reminder(conference, recipients=None, field_name="week_before", update_setting=True):
     """
     Send conference reminder email a week/day/hour before start
     """
+    def render_template(extra_fields, field_name, field_type, context):
+        template = extra_fields.get(f'reminder_{field_name}_{field_type}')
+        template = template or get_initial_template(f'{field_name}_{field_type}')
+        template = re.sub('\%\(([^\)]+)\)', r'{{\1}}', template)
+        return Template(template).render(Context(context))
     if not recipients:
         recipients = conference.users.filter(is_active=True)
     for recipient in recipients:
         extra_fields = conference.extra_fields or {}
-        context = Context({'conference': conference})
+        context = get_common_mail_context(request=None, group=conference, user=recipient)
 
-        subject = extra_fields.get(f'reminder_{field_name}_subject')
-        subject = subject or get_initial_template(f'{field_name}_subject')
-        subject = Template(subject).render(context)
-        content = extra_fields.get(f'reminder_{field_name}_content')
-        content = content or get_initial_template(f'{field_name}_content')
-        content = Template(content).render(context)
-
+        subject = render_template(extra_fields, field_name, 'subject', context)
+        content = render_template(extra_fields, field_name, 'content', context)
         send_html_mail_threaded(recipient, subject, content)
 
     # Update sent setting
