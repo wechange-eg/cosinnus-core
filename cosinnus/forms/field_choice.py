bc522993de905bb645bb88b76ad86fee49360e6f
Tobias Dietze <tobias.dietze@opox.org>
2020-12-01 14:37:33 +0100

    added form templates
    * added dynamic field form class
    * added dynamic field form templates

--- cosinnus/forms/field_choice.py
+++ cosinnus/forms/field_choice.py
@@ -1,22 +1,45 @@
 from cosinnus.dynamic_fields.dynamic_formfields import DynamicFieldFormFieldGenerator
-from django.forms import forms
+from django import forms
 from cosinnus.dynamic_fields.quick_import import BOELL_USERPROFILE_EXTRA_FIELDS
+from cosinnus.dynamic_fields.dynamic_fields import DYNAMIC_FIELD_TYPE_ADMIN_DEFINED_CHOICES_TEXT
+
+
+def get_dynamic_admin_field_namess():
+    """ get all fields with type of DYNAMIC_FIELD_TYPE_ADMIN_DEFINED_CHOICES_TEXT """
+    field_list = []
+
+    for field_name, field in BOELL_USERPROFILE_EXTRA_FIELDS.items():
+        if field.type == DYNAMIC_FIELD_TYPE_ADMIN_DEFINED_CHOICES_TEXT:
+            field_list.add(field_name)
+
+    return field_list
+
 
 class DynamicFieldForm(forms.Form):
+    options = forms.CharField(max_length=300)
+
+    def clean(self):
+        data = self.cleaned_data
+
+    def clean_options(self):
+        pass
+
+    def save(self):
+        self._cosinnus_portal.dynamic_field_choices[self._dynamic_field_name] = self.cleaned_data['options']
+
     def __init__(self, cosinnus_portal, dynamic_field_name, *args, **kwargs):
-
-        field_choices = cosinnus_portal.dynamic_field_choices.get(dynamic_field_name, [])
-        field_generator = DynamicFieldFormFieldGenerator()
-        cosinnus_field = self.get_cosinnus_dynamic_field(dynamic_field_name)
-        field = field_generator.get_formfield(dynamic_field_name, cosinnus_field)
-
         super(DynamicFieldForm, self).__init__(*args, **kwargs)
+
+        self._dynamic_field_name = dynamic_field_name
+        self._cosinnus_portal = cosinnus_portal
+
+        self._field_choices = cosinnus_portal.dynamic_field_choices.get(dynamic_field_name, [])
 
     def get_cosinnus_dynamic_field(self, field_name):
         field = BOELL_USERPROFILE_EXTRA_FIELDS.get(field_name, "")
-        if not field:
-            raise AttributeError("%s is not a defined field in BOELL_USERPROFILE_EXTRA_FIELDS")
+        if not field and not getattr(field, 'type', None) == DYNAMIC_FIELD_TYPE_ADMIN_DEFINED_CHOICES_TEXT:
+            raise AttributeError("%s is not a defined field in BOELL_USERPROFILE_EXTRA_FIELDS or nto type of DYNAMIC_FIELD_TYPE_ADMIN_DEFINED_CHOICES_TEXT")
         return field
-
-    def add_choice(self, data):
-        pass
+    #
+    # def add_choice(self, data):
+    #     pass
---
+++ cosinnus/views/dynamic_fields.py
@@ -0,0 +1,26 @@
+from django.views.generic.base import TemplateView
+from django.shortcuts import redirect, render
+from django.utils.translation import ugettext as _
+
+from cosinnus.forms.field_choice import *
+from cosinnus.models import CosinnusPortal
+
+
+class DynamicFieldFormView(TemplateView):
+    template_name = 'cosinnus/dynamic_fields/dynamic_field_form.html'
+    form_class = DynamicFieldForm
+
+    # TODO change method to get the default portal
+    default_portal = CosinnusPortal.objects.first()
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, template_name=self.template_name, context={'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(data=request.POST)
+
+        if form.is_valid():
+            form.save()
+
+        return render(request, template_name=self.template_name, context={'form': form})
