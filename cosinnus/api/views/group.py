bde0f880d58857c5b91d58a6223370fc26d2547a
Simon Liedtke <simon.liedtke@gmail.com>
2021-04-24 12:26:59 +0200

    Allow creating groups/projects for admin users

--- cosinnus/api/views/group.py
+++ cosinnus/api/views/group.py
@@ -1,18 +1,22 @@
 from rest_framework import viewsets
 
 from cosinnus.api.serializers.group import CosinnusSocietySerializer, CosinnusProjectSerializer
-from cosinnus.api.views.mixins import PublicCosinnusGroupFilterMixin, CosinnusFilterQuerySetMixin
+from cosinnus.api.views.mixins import PublicCosinnusGroupFilterMixin, CosinnusFilterQuerySetMixin, ReadOnlyOrIsAdminUser
+from cosinnus.conf import settings
 from cosinnus.models.group_extra import CosinnusSociety, CosinnusProject
 
 
 class CosinnusSocietyViewSet(CosinnusFilterQuerySetMixin,
                              PublicCosinnusGroupFilterMixin,
-                             viewsets.ReadOnlyModelViewSet):
+                             viewsets.ModelViewSet):
+    http_method_names = getattr(settings, 'COSINNUS_API_SETTINGS', {}).get('society', ['get', ])
+    permission_classes = (ReadOnlyOrIsAdminUser,)
     queryset = CosinnusSociety.objects.all()
     serializer_class = CosinnusSocietySerializer
 
 
 class CosinnusProjectViewSet(CosinnusSocietyViewSet):
-
+    http_method_names = getattr(settings, 'COSINNUS_API_SETTINGS', {}).get('project', ['get', ])
+    permission_classes = (ReadOnlyOrIsAdminUser,)
     queryset = CosinnusProject.objects.all()
     serializer_class = CosinnusProjectSerializer
