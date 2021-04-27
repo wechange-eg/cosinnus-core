b4adc54c9c6969364ab14cfc64ab922f9353eb7e
Simon Liedtke <simon.liedtke@gmail.com>
2021-04-27 16:07:24 +0200

    Added related/parent fields to group API

--- cosinnus/api/views/group.py
+++ cosinnus/api/views/group.py
@@ -3,8 +3,11 @@ from rest_framework import viewsets
 from cosinnus.api.serializers.group import CosinnusSocietySerializer, CosinnusProjectSerializer
 from cosinnus.api.views.mixins import PublicCosinnusGroupFilterMixin, CosinnusFilterQuerySetMixin, ReadOnlyOrIsAdminUser
 from cosinnus.conf import settings
+from cosinnus.models import RelatedGroups
 from cosinnus.models.group_extra import CosinnusSociety, CosinnusProject
+from cosinnus.utils.group import get_cosinnus_group_model
 
+CosinnusGroup = get_cosinnus_group_model()
 
 class CosinnusSocietyViewSet(CosinnusFilterQuerySetMixin,
                              PublicCosinnusGroupFilterMixin,
@@ -13,6 +16,17 @@ class CosinnusSocietyViewSet(CosinnusFilterQuerySetMixin,
     permission_classes = (ReadOnlyOrIsAdminUser,)
     queryset = CosinnusSociety.objects.all()
     serializer_class = CosinnusSocietySerializer
+
+    def perform_create(self, serializer):
+        super().perform_create(serializer)
+        # Add related groups
+        for related_slug in self.request.data.get('related', []):
+            related_group = CosinnusGroup.objects.filter(slug=related_slug).first()
+            if not related_group:
+                continue
+            RelatedGroups.objects.get_or_create(from_group=serializer.instance, to_group=related_group)
+            RelatedGroups.objects.get_or_create(from_group=related_group, to_group=serializer.instance)
+
 
 
 class CosinnusProjectViewSet(CosinnusSocietyViewSet):
