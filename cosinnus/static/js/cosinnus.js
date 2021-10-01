7179be8f67ad5065bebb9c0bb2199fa41ebc3a96
Magdalena Noffke <magdalena.noffke@googlemail.com>
2021-10-01 11:36:56 +0200

    Set longer delay for touch

--- cosinnus/static/js/cosinnus.js
+++ cosinnus/static/js/cosinnus.js
@@ -279,7 +279,7 @@
                         fixedWeekCount: false,
                         editable: editable,
                         events: cosinnus_calendarEvents,
-                        selectLongPressDelay: 0,
+                        selectLongPressDelay: 300,
                         timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
                         eventDrop: function(date) {
                             $(calendarEl)
@@ -326,7 +326,7 @@
                         contentHeight: 'auto',
                         eventDisplay: 'background',
                         selectable: true,
-                        selectLongPressDelay: 0,
+                        selectLongPressDelay: 300,
                         timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
                         dateClick: function(date) {
                             $(calenderEl).trigger('fullCalendarDayClick',[date]);
