5d502de5898e49eaf93220bff735324d0c2d1d12
Sascha Narr <saschanarr@gmail.com>
2021-10-05 16:29:48 +0200

    Fix navbar/header URL as non-capturing regex

--- cosinnus/urls.py
+++ cosinnus/urls.py
@@ -361,7 +361,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns += [
-    url(r'^api/v2/(header|navbar)/$', header, name='api-header'),
+    url(r'^api/v2/(?:header|navbar)/$', header, name='api-header'),
     url(r'^api/v2/footer/$', footer, name='api-footer'),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
