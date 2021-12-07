81417b8cba7f08f1632c5a675e5a2fcc53a89f5a
Rashid <rashid.rysaev93@gmail.com>
2021-12-07 12:00:02 +0100

    Some changes into the view added

--- cosinnus/views/authentication.py
+++ cosinnus/views/authentication.py
@@ -77,8 +77,7 @@ class UserOTPTokenValidationView(RequireLoggedInMixin, auth_views.LoginView):
         return partial(self.otp_token_form, self.request.user)
     
     def get_success_url(self):
-        url = self.get_redirect_url()
-        return url or safe_redirect(request=self.request.GET.get('next', get_non_cms_root_url(self.request)))
+        return safe_redirect(self.request.GET.get('next', get_non_cms_root_url(self.request)), self.request)
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
