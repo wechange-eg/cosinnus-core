a8af376aad074732923af440505c5ed255b4793e
Rashid <rashid.rysaev93@gmail.com>
2021-12-17 13:53:42 +0100

    Code cleaned up; some small changed added

--- cosinnus/core/middleware/time_zone_middleware.py
+++ cosinnus/core/middleware/time_zone_middleware.py
@@ -4,6 +4,10 @@ from django.utils.deprecation import MiddlewareMixin
 
 
 class TimezoneMiddleware(MiddlewareMixin):
+    """
+        The Middleware handles the time settings by setting the time zone chosen by user
+        as default for the entire portal.
+    """
 
     def process_request(self, request):
         if request.user.is_authenticated:
--- cosinnus/static/js/cosinnus.js
+++ cosinnus/static/js/cosinnus.js
@@ -783,7 +783,9 @@
 
         renderMomentDataDate : function() {
 
-            var defaultTimeZone = moment.tz.setDefault(cosinnus_user_timezone);
+            if (cosinnus_user_timezone){
+                moment.tz.setDefault(cosinnus_user_timezone)
+            }
             
             // when .moment-data-date elements have a data-date attribute, render date.
             $('.moment-data-date').on("renderMomentDataDate", function() {
@@ -801,7 +803,6 @@
                 if (data_date == 'today') {
                     // if attribute is 'today', fill with current date
                     // if it is not 'today', it is 2014-04-28.
-                    data_date = defaultTimeZone;
                     data_date = data_date.getFullYear() + "-"
                         + ((data_date.getMonth()+1).toString().length === 2
                             ? (data_date.getMonth()+1)
@@ -853,7 +854,7 @@
         renderTimezoneAwareDates: function () {
 
             $('.moment-timezone-aware-date').each(function() {
-                var browserTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+                var browserTimezone = cosinnus_user_timezone || Intl.DateTimeFormat().resolvedOptions().timeZone
 
                 var fromDate = $(this).attr('data-from-date')
                 var toDate = $(this).attr('data-to-date')
