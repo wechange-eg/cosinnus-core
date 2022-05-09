874c25ec678a0297138c6e496e60d93d7ab8a054
Magdalena Noffke <magdalena.noffke@googlemail.com>
2022-05-09 13:55:59 +0200

    Add chakra to project

--- cosinnus_frontend/src/index.tsx
+++ cosinnus_frontend/src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from "react-redux"
-
+import { ChakraProvider } from '@chakra-ui/react'
 import {rootStore} from "./stores/rootStore"
 import {App} from "./views/App"
 
 ReactDOM.render(
   <Provider store={rootStore}>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </Provider>,
   document.getElementById("app")
 )
--- cosinnus_frontend/src/views/components/Login/style.tsx
+++ cosinnus_frontend/src/views/components/Login/style.tsx
@@ -1,65 +1,13 @@
 import React from "react"
-import { Box, Button, Grid, Typography } from "@material-ui/core"
-import { styled, makeStyles } from "@material-ui/core/styles"
-import { Form } from "formik"
-import { TextField } from "formik-material-ui"
+import { FieldHookConfig, useField } from "formik";
+import {
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
 
-const imgType = (<img/>).type;
+type Props = ChakraInputProps & FieldHookConfig<"input">;
 
-export const LoginButton = styled(Button)({
-  marginBottom: 8
-});
-
-export const LoginForm = styled(Form)({
-
-});
-
-export const LoginGrid = styled(Grid)({
-  display: "flex",
-  padding: 100
-});
-
-export const LoginImg = styled(imgType)({
-  display: "block",
-  margin: "0 auto 4rem",
-  height: "3rem",
-  padding: "0 4rem"
-});
-
-export const LoginOverlay = styled(Box)({
-  position: "absolute",
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  flexDirection: "column",
-  backdropFilter: "blur(5px)",
-  zIndex: 1000
-});
-
-export const LoginProgressTypography = styled(Typography)({
-  marginTop: 8
-});
-
-export const LoginTextField = styled(TextField)(({ theme }) => ({
-  minWidth: 250,
-  background: theme.palette.background.paper,
-  borderRadius: theme.shape.borderRadius,
-  marginBottom: 8
-}));
-
-export const useInputPropsStyles = makeStyles(theme => ({
-  underline: {
-    "&&&:before": {
-      borderBottom: "none"
-    },
-    "&&:after": {
-      borderBottom: "none"
-    },
-    borderRadius: 4,
-    background: theme.palette.background.paper
-  }
-}));
+export const LoginTextField = ({ name, ...props }: Props) => {
+  const [field] = useField(name)
+  return <ChakraInput {...props} {...field} />
+}
