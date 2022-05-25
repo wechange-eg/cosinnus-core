1ac8034580699741917b71b714bdfe9fa29ce881
Magdalena Noffke <magdalena.noffke@googlemail.com>
2022-05-25 16:17:51 +0200

    Remove Material UI components

--- cosinnus_frontend/src/views/Map/style.tsx
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from '@material-ui/core/styles'
-
-import {miniSidebarWidth, sidebarWidth} from "../../../themes/themes"
-
-export const useStyles = makeStyles((theme) => ({
-  grid: {
-    height: "100%"
-  }
-}))
--- cosinnus_frontend/src/views/components/ResultMap/style.tsx
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from '@material-ui/core/styles';
-
-export const useStyles = makeStyles((theme) => ({
-  map: {
-    flex: 1,
-    height: "100%"
-  }
-}))
