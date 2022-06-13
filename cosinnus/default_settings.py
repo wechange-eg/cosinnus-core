0e397d28a61047f7f46457ca736e572f87f4bfaa
Sascha Narr <saschanarr@gmail.com>
2022-06-13 12:33:43 +0200

    Very cheap fast path change

--- cosinnus/default_settings.py
+++ cosinnus/default_settings.py
@@ -646,10 +646,10 @@ COSINNUS_EXCHANGE_BACKENDS = []
 
 # Frontend
 COSINNUS_FRONTEND_ENABLED = False
-with open(join(BASE_PATH, 'cosinnus_frontend', 'urls')) as f:
+with open(join(BASE_PATH, 'cosinnus/cosinnus_frontend', 'urls')) as f:
     COSINNUS_FRONTEND_URL_PATTERNS = [f.strip() for f in f]
-with open(join(BASE_PATH, 'cosinnus_frontend', 'assets')) as f:
+with open(join(BASE_PATH, 'cosinnus/cosinnus_frontend', 'assets')) as f:
     COSINNUS_FRONTEND_ASSET_PATTERNS = [f.strip() for f in f]
-COSINNUS_FRONTEND_ROOT = join(BASE_PATH, 'cosinnus_frontend', 'dist')
+COSINNUS_FRONTEND_ROOT = join(BASE_PATH, 'cosinnus/cosinnus_frontend', 'dist')
 COSINNUS_FRONTEND_PATH = 'index.html'
 
