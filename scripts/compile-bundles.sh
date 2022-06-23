a43476b94f18458b681772eea13bc3abd07332da
Sascha Narr <saschanarr@gmail.com>
2022-06-23 17:36:25 +0200

    Adjusted compile-bundles.sh - removed LESS compile.
    Because it should be compiled on a per-portal basis at deploy time after all.

--- scripts/compile-bundles.sh
+++ scripts/compile-bundles.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-echo "This script compiles all cosinnus JS/LESS bundles. Call this script from your wechange directory (the one containing 'manage.py'), like this: '../cosinnus-core/scripts/compile-static-bundles.sh'"
-
-# less
-./manage.py collectstatic --noinput
-../cosinnus-core/node_modules/.bin/lessc --clean-css ./static-collected/less/cosinnus.less ../cosinnus-core/cosinnus/static/css/cosinnus.css
-echo "Built cosinnus/static/css/cosinnus.css."
+echo "This script compiles all cosinnus JS bundles. Call this script from your wechange directory (the one containing 'manage.py'), like this: '../cosinnus-core/scripts/compile-static-bundles.sh'"
 
 cd ../cosinnus-core/
 # run 'npm install' in this folder the first time you do this!
