8f62dfb306c0d03c1b68769f4b14b25dffa9d66f
Sascha Narr <saschanarr@gmail.com>
2022-07-21 16:53:55 +0200

    v1.0.5. Added basic swagger docs for api v3 /signup/ and /login/

--- cosinnus/api_frontend/views/user.py
+++ cosinnus/api_frontend/views/user.py
@@ -16,6 +16,8 @@ from cosinnus.utils.permissions import IsNotAuthenticated
 from cosinnus.views.user import UserSignupTriggerEventsMixin
 from rest_framework.renderers import BrowsableAPIRenderer
 from cosinnus.api_frontend.handlers.renderers import CosinnusAPIFrontendJSONResponseRenderer
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
 class LoginView(LoginViewAdditionalLogicMixin, APIView):
@@ -24,7 +26,42 @@ class LoginView(LoginViewAdditionalLogicMixin, APIView):
     # disallow logged in users
     permission_classes = (IsNotAuthenticated,)
     renderer_classes = (BrowsableAPIRenderer, CosinnusAPIFrontendJSONResponseRenderer,)
-
+    
+    # todo: generate proper response, by either putting the entire response into a
+    #       Serializer, or defining it by hand
+    #       Note: Also needs docs on our custom data/timestamp/version wrapper!
+    # see:  https://drf-yasg.readthedocs.io/en/stable/custom_spec.html
+    # see:  https://drf-yasg.readthedocs.io/en/stable/drf_yasg.html?highlight=Response#drf_yasg.openapi.Schema
+    @swagger_auto_schema(
+        request_body=CosinnusUserLoginSerializer,
+        responses={'200': openapi.Response(
+            description='WIP: Response info missing. Short example included',
+            examples={
+                "application/json": {
+                    "data": {
+                        "refresh": "eyJ...",
+                        "access": "eyJ...",
+                        "user": {
+                            "id": 77,
+                            "username": "77",
+                            "first_name": "NewUser",
+                            "last_name": "",
+                            "profile": {
+                                "id": 82,
+                                "avatar": None,
+                                "avatar_80x80": None,
+                                "avatar_50x50": None,
+                                "avatar_40x40": None
+                            }
+                        },
+                        "next": "/dashboard/"
+                    },
+                    "version": "1.0.4",
+                    "timestamp": 1658414865.057476
+                }
+            }
+        )}
+    )
     def post(self, request):
         serializer = CosinnusUserLoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -50,13 +87,49 @@ class LoginView(LoginViewAdditionalLogicMixin, APIView):
         return response
 
 
+@swagger_auto_schema(request_body=CosinnusUserSignupSerializer)
 class SignupView(UserSignupTriggerEventsMixin, APIView):
     """ A proper User Registration API endpoint """
     
     # disallow logged in users
     permission_classes = (IsNotAuthenticated,)
     renderer_classes = (BrowsableAPIRenderer, CosinnusAPIFrontendJSONResponseRenderer,)
-
+    
+    # todo: generate proper response, by either putting the entire response into a
+    #       Serializer, or defining it by hand
+    #       Note: Also needs docs on our custom data/timestamp/version wrapper!
+    # see:  https://drf-yasg.readthedocs.io/en/stable/custom_spec.html
+    # see:  https://drf-yasg.readthedocs.io/en/stable/drf_yasg.html?highlight=Response#drf_yasg.openapi.Schema
+    @swagger_auto_schema(
+        request_body=CosinnusUserSignupSerializer,
+        responses={'200': openapi.Response(
+            description='WIP: Response info missing. Short example included',
+            examples={
+                "application/json": {
+                    "data": {
+                        "user": {
+                            "id": 90,
+                            "username": "90",
+                            "first_name": "ApiUserFirst",
+                            "last_name": "ApIuserLast",
+                            "profile": {
+                                "id": 95,
+                                "avatar": None,
+                                "avatar_80x80": None,
+                                "avatar_50x50": None,
+                                "avatar_40x40": None
+                            }
+                        },
+                        "next": "/dashboard/",
+                        "refresh": "eyJ...",
+                        "access": "eyJ..."
+                    },
+                    "version": "1.0.4",
+                    "timestamp": 1658415026.545203
+                }
+            }
+        )}
+    )
     def post(self, request):
         serializer = CosinnusUserSignupSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
