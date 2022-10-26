7326194301d3cbb5394c6e360129e29111d3a262
Sascha Narr <saschanarr@gmail.com>
2022-10-26 15:16:23 +0200

    v1.1.6: config: add venv locale directory to LOCALE_PATHS

--- cosinnus/default_settings.py
+++ cosinnus/default_settings.py
@@ -92,6 +92,7 @@ def define_cosinnus_base_settings(project_settings, project_base_path):
     LOCALE_PATHS = [
         join(COSINNUS_BASE_PATH, 'locale'),
         join(BASE_PATH, "locale"),
+        join(BASE_PATH, ".venv", "locale"),
         join(BASE_PATH, "apps", "core", "locale"),
     ]
     
