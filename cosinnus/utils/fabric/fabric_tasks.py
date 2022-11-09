774269a096bffd431fae82882044e2ddda4b2124
Sascha Narr <saschanarr@gmail.com>
2022-11-09 14:58:18 +0100

    Added refactored fabric_tasks for fabric2 format

---
+++ cosinnus/utils/fabric/fabric_tasks.py
@@ -0,0 +1,338 @@
+"""
+    Cosinnus fabric tasks definitions.
+    
+    How to use: define a `fabfile.py` in your project with one task per target server. Example fabfile.py:
+    
+    ```
+    from fabric import task
+    from cosinnus.utils.fabric.fabric_utils import CosinnusFabricConnection, setup_env
+    from cosinnus.utils.fabric.fabric_tasks import *
+    
+    @task
+    def staging(_ctx):
+        env = setup_env(
+            portal_name='my_portal',
+            domain='my_portal.de',
+            pull_branch='my_branch',
+        )
+    ```
+    
+    Then invoke any task defined in this file by calling `fab`. 
+    Usually you want to only call the high level tasks, (see "Deployment tasks" below),
+    unless you want to chain individual small tasks to perform a custom execution.
+    
+    Examples:
+    
+    `fab staging hotdeploy` (fast deploy for small changes)
+    
+    `fab staging deploy` (proper deploy with downtime banner)
+    
+    `fab staging pull restart` (custom execution)
+"""
+
+from cosinnus.utils.fabric.fabric_utils import cosinnus_fabric_task as task
+from cosinnus.utils.fabric.fabric_utils import get_env, CosinnusFabricConnection
+
+# Sudo cd context https://stackoverflow.com/questions/50834162/in-fabric-2-invoke-change-directory-and-use-sudo
+"""
+# Normal task with cd
+@task
+def foo(context):
+    with context.cd('/'):
+        context.run('pwd')
+
+"""
+
+# virtualenv: https://stackoverflow.com/questions/53182539/conda-virtual-environment-with-fabric2
+"""
+@task(hosts=['servername'])
+def do_things(c):
+    with c.cd('your_dir'):
+        # assuming you already added myenv to your path 
+        with c.prefix('source activate myenv'): 
+            c.run('pip3.6 install -r requirements.txt') #for example if you have
+
+"""
+
+
+""" ----------- Deployment tasks ----------- """
+
+@task
+def deploy(_ctx, do_maintenance=True):
+    """ 
+        Main deploy command, full deploy with dependency update, raising a downtime banner
+        on the entire site, blocking any user access.
+        
+        Will do the following in order:
+            * pull up a maintenance notice page
+            * stop the servers
+            * do a DB backup
+            * pull from git
+            * pip update
+            * migrate
+            * start the servers
+            * remove the maintenance notice page 
+    """
+    check_confirmation()
+    backup(_ctx)
+    if do_maintenance:
+        maintenanceon(_ctx)
+    stop(_ctx)
+    _pull_and_update()
+    # restart_db()
+    migrate(_ctx)
+    start(_ctx)
+    compilewebpack(_ctx)
+    collectstatic(_ctx)
+    compileless(_ctx)
+    if do_maintenance:
+        maintenanceoff(_ctx)
+
+@task
+def hotdeploy(_ctx):
+    """ Fast deploy with poetry update and soft server restarts. No downtime-banner raised.
+        Recommended only for hotfixes that do not require migrations,
+        else you run the risk that active users get server errors while the deploy is running. """
+    check_confirmation()
+    _pull_and_update()
+    migrate(_ctx)
+    restart(_ctx)
+    compilewebpack(_ctx)
+    collectstatic(_ctx)
+    compileless(_ctx)
+
+
+@task
+def fastdeploy(_ctx):
+    """ Fast deploy with soft server restarts. No migrating or poetry update done."""
+    fastpull(_ctx)
+    restart(_ctx)
+    collectstatic(_ctx)
+    compileless(_ctx)
+
+
+@task
+def fastpull(_ctx):
+    """ Only does a git pull on the base project repository """
+    check_confirmation()
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    with c.cd(env.path):
+        c.run(f'git checkout {env.pull_branch}')
+        c.run(f'git pull')
+
+
+""" ----------- Single helper tasks. Used in deploy tasks, but can also be called solo ----------- """
+
+def check_confirmation():
+    """ Blocks execution with a shell confirmation prompt """
+    env = get_env()
+    if env.confirm:
+        text = input(
+            f'\n     **************   WARNING!   ************* \n\nYou are about to deploy to the Server {env.username}@{env.host}.\nIf you are sure you want this, type "YES" to continue: '
+        )
+        if not text == 'YES':
+            print('Canceled deploy.')
+            exit()
+    
+@task
+def restart(_ctx):
+    """ Restart the django service """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    c.run(env.reload_command)
+
+@task
+def stop(_ctx):
+    """ Stop the django service """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    c.run(env.stop_command)
+
+@task
+def start(_ctx):
+    """ Start the django service """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    c.run(env.start_command)
+
+@task
+def restartmemcached(_ctx):
+    """ Restart the django service """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    c.run(env.memcached_restart_command)
+
+@task
+def maintenanceon(_ctx):
+    """ Turn on maintenance mode by moving the maintenance.html file
+        where nginx will see it and return only that file as response. """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    with c.cd(env.maintenance_mode_path):
+        c.run('mv maintenance.html.off maintenance.html')
+
+@task
+def maintenanceoff(_ctx):
+    """ Turn off maintenance mode by moving the maintenance.html file
+        where nginx won't see it anymore. """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    with c.cd(env.maintenance_mode_path):
+        c.run('mv maintenance.html maintenance.html.off')
+    
+@task
+def migrate(_ctx):
+    """ Run a django migrate """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    with c.prefix(f'source {env.virtualenv_path}/bin/activate'):
+        c.run(f'{env.path}/manage.py migrate --fake-initial')
+    
+@task
+def showmigrations(_ctx):
+    """ Run a django showmigrations """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    with c.prefix(f'source {env.virtualenv_path}/bin/activate'):
+        c.run(f'{env.path}/manage.py showmigrations')
+
+@task
+def collectstatic(_ctx):
+    """ Run a django collectstatic """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    with c.cd(env.path):
+        with c.prefix(f'source {env.virtualenv_path}/bin/activate'):
+            c.run('./manage.py collectstatic --noinput')
+
+@task
+def staticown(_ctx):
+    """ Chowns all media files and collected-static files. Useful only after a portal transfer/copy """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    with c.cd(env.path):
+        c.run(f'chown -R {env.username}:www-data ./static-collected')
+        c.run(f'chown -R {env.username}:www-data ./media')
+
+@task
+def compileless(_ctx):
+    """ Compiles all LESS files and adds the resulting CSS file to the collected-static folder """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    with c.cd(env.path):
+        c.run(
+            '~/node_modules/.bin/lessc --clean-css ./static-collected/less/cosinnus.less ./static-collected/css/cosinnus.css'
+        )
+        for less_file in env.portal_additional_less_to_compile:
+            c.run(f'~/node_modules/.bin/lessc --clean-css ./static-collected/less/{less_file}.less ./static-collected/css/{less_file}.css')
+
+@task
+def compilewebpack(_ctx):
+    """ Compiles the map/conferences JS clients using webpack """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    with c.prefix(f'source {env.virtualenv_path}/bin/activate'):
+        # copy the main project PO files to cosinnus-core/cosinnus/locale-extra
+        # for compilation of the JS app translations
+        c.run(f'mkdir -p {env.virtualenv_path}/src/cosinnus/cosinnus/locale_extra')
+        if env.legacy_mode:
+            c.run(f'cp -R {env.path}/wechange/locale/* {env.virtualenv_path}/src/cosinnus/cosinnus/locale_extra/')
+        else:
+            c.run(f'cp -R {env.path}/apps/core/locale/* {env.virtualenv_path}/src/cosinnus/cosinnus/locale_extra/')
+        with c.cd(f'{env.virtualenv_path}/src/cosinnus/'):
+            c.run('npm install')
+            c.run('npm run production')  # -->can also run 'npm run dev', but it stays in watch mode
+        # build conference frontend
+        with c.cd(f'{env.virtualenv_path}/src/cosinnus/cosinnus_conference/frontend/'):
+            c.run('npm install')
+            c.run('npm run build')
+
+
+@task
+def compilehtmlemails(_ctx):
+    """ This generates a project-specific set of HTML email templates that overwrite the built ones
+        in cosinnus-core (templates/cosinnus/html_mail).
+        By adding files with the same structure as the npm email-builder inside cosinnus-core/cosinnus/html_emails/,
+        you can customize the email templates for this portal only.
+        It is recommended to only replace the /src/assets/scss/_extra_scss.scss and _extra_variables.scss files! """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    with c.prefix(f'source {env.virtualenv_path}/bin/activate'):
+        # ensure all directories exist
+        c.run(f'mkdir -p {env.path}/html_emails_collected')
+        c.run(f'mkdir -p {env.path}/wechange/templates/cosinnus/html_mail')
+        # copy cosinnus-core html emails npm project
+        c.run(f'cp -R {env.virtualenv_path}/src/cosinnus/cosinnus/html_emails/* {env.path}/html_emails_collected/')
+        # copy local repo overriding files for html emails npm project, overwriting files
+        result = c.run(f'cp -R {env.path}/wechange/html_emails/* {env.path}/html_emails_collected/', warn=True)
+        if result.failed: 
+            print('Project does not seem to specify overriding email templates/styles. Ignoring build and continuing fabfile.')
+            return
+        with c.cd(f'{env.path}/html_emails_collected/'):
+            # build the html emails
+            result = c.run('npm run build', warn=True) # -->can also run 'npm run dev', but it stays in watch mode
+            # if we fail here, the npm project has probably never been set up before
+            if result.failed: 
+                print('Error during npm run, trying npm install in case it has not been installed yet.')
+                c.run('npm install')
+                c.run('npm run build') # -->can also run 'npm run dev', but it stays in watch mode
+            # copy the built email templates to our local repo template folder, where they path-replace the cosinnus-core ones
+            c.run('cp -R %(path)s/html_emails_collected/dist/{digest.html,notification.html,summary_group.html,summary_item.html}* %(path)s/wechange/templates/cosinnus/html_mail/' % env)
+
+@task
+def backup(_ctx):
+    """ Saves a dump of the current django psql database and places it into ~/backups/ """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    with c.cd(env.backup_path):
+        c.run(f'pg_dump -Fc -U {env.db_username} {env.db_name} > {env.db_name}_backup_$(date +%%F-%%T).sql')
+        c.run('ls -lt')
+
+@task
+def rebuildindex(_ctx):
+    """ Runs a django elasticsearch rebuild index, deleting the current index """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    check_confirmation()
+    with c.prefix(f'source {env.virtualenv_path}/bin/activate'):
+        c.run(f'{env.path}/manage.py rebuild_index -v 2')
+
+@task
+def updateindex(_ctx):
+    """ Runs a django elasticsearch update index, deleting the current index """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    check_confirmation()
+    with c.prefix(f'source {env.virtualenv_path}/bin/activate'):
+        c.run(f'{env.path}/manage.py update_index -v 2')
+
+@task
+def pipfreeze(_ctx):
+    """ Run a pip freeze """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    with c.cd(env.path):
+        with c.prefix(f'source {env.virtualenv_path}/bin/activate'):
+            c.run('pip freeze')
+
+def _pull_and_update(full_update=True):
+    """ 
+        Does a git pull on the main project repository, then performs 
+        a poetry update to update dependencies.
+        (Not a task, this is a helper function called from other tasks.) 
+    """
+    env = get_env()
+    c = CosinnusFabricConnection(host=env.host)
+    with c.cd(env.path):
+        c.run(f'git checkout {env.pull_branch}')
+        c.run(f'git pull')
+        with c.prefix(f'source {env.virtualenv_path}/bin/activate'):
+            if env.legacy_mode:
+                c.run(f'pip install -Ur {env.special_requirements}')
+            else:
+                if full_update:
+                    c.run('poetry update')
+                else:
+                    c.run('poetry update cosinnus')
+
---
+++ cosinnus/utils/fabric/fabric_utils.py
@@ -0,0 +1,135 @@
+from fabric import Connection, task
+from django.core.exceptions import ImproperlyConfigured
+import functools
+
+# the env used for all cosinnus fabric connections
+_env = None
+
+
+class CosinnusFabricConnection(Connection):
+    """ Wrapper for the fabric Connection object that prints commands to stdout
+        as they are being executed. """
+    
+    def run(self, *args, **kwargs):
+        print(f'>>> run\t {args[0]}\n')
+        return super().run(*args, **kwargs)
+    
+    def cd(self, *args, **kwargs):
+        print(f'cd:\t {args[0]}')
+        return super().cd(*args, **kwargs)
+    
+    def prefix(self, *args, **kwargs):
+        print(f'with:\t {args[0]}')
+        return super().prefix(*args, **kwargs)
+
+
+def cosinnus_fabric_task(func, **kwargs):
+    """
+        Wrapper for fabric.task that prints out the task about to execute to stdout.
+    """
+    func_name = func.__name__
+    @functools.wraps(func)
+    def wrapper(ctx, **kwargs):
+        print(f'\n##########  Task: {func_name}  ##########\n')
+        return func(ctx, **kwargs)
+    return task(wrapper, **kwargs)
+
+
+class _AttributeDict(dict):
+    """
+    Dictionary subclass enabling attribute lookup/assignment of keys/values.
+    Taken over from fabric 1.*
+
+    For example::
+
+        >>> m = _AttributeDict({'foo': 'bar'})
+        >>> m.foo
+        'bar'
+        >>> m.foo = 'not bar'
+        >>> m['foo']
+        'not bar'
+
+    ``_AttributeDict`` objects also provide ``.first()`` which acts like
+    ``.get()`` but accepts multiple keys as arguments, and returns the value of
+    the first hit, e.g.::
+
+        >>> m = _AttributeDict({'foo': 'bar', 'biz': 'baz'})
+        >>> m.first('wrong', 'incorrect', 'foo', 'biz')
+        'bar'
+
+    """
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            # to conform with __getattr__ spec
+            raise AttributeError(key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def first(self, *names):
+        for name in names:
+            value = self.get(name)
+            if value:
+                return value
+
+
+def get_env():
+    """ Returns the env used for all cosinnus fabric connections """
+    global _env
+    if _env is None:
+        _env = _AttributeDict({})
+    return _env
+
+
+def setup_env(portal_name, domain, pull_branch, confirm=False, 
+              base_path=None, pull_remote='origin',
+              legacy_mode=False, special_requirements='requirements-production.txt'):
+    """ 
+        Sets up the env with all variables needed to run cosinnus 
+        fabric commands.
+        @param portal_name: the name for the user and portal instance
+        @param domain: the URL domain for the portal, for SSH connections and 
+            on-server paths
+        @param pull_branch: the branch that the main repo will be pulled from
+        @param base_path: if the on-server file location path for the portal
+            is non-default, use this argument to override it
+        @param pull_remote: used to override non-origin git pulls
+        @param legacy_mode: set this to True for deploys on portals that 
+            do not have the v3 redesign devops architecture yet (pip instead of poetry, etc)
+        @param special_requirements: if run in `legacy_mode == True`, this can be used to change
+            the requirements file
+    """
+    if not base_path and domain:
+        base_path = f'/srv/http/{domain}'
+    #if not base_path:
+    #    raise ImproperlyConfigured('`setup_env()` did not receive all necessary arguments!')
+    
+    env = get_env()
+    env.portal_name = portal_name
+    env.username = portal_name
+    env.host = f'{portal_name}@{domain}'
+    env.path = f'{base_path}/htdocs'
+    env.virtualenv_path = f'{env.path}/.venv'
+    env.backup_path = f'{base_path}/backups'
+    env.maintenance_mode_path = base_path
+    env.pull_branch = pull_branch
+    env.pull_remote = pull_remote
+    env.reload_command = f'sudo /bin/systemctl restart django-{portal_name}.service'
+    env.stop_command = f'sudo /bin/systemctl stop django-{portal_name}.service'
+    env.start_command = f'sudo /bin/systemctl start django-{portal_name}.service'
+    env.memcached_restart_command = f'sudo /bin/systemctl restart django-{portal_name}-memcached.service'
+    env.portal_additional_less_to_compile = [] # a list of django apps for which to compile extra less
+    env.db_name = portal_name
+    env.db_username = portal_name
+    env.confirm = confirm
+    env.legacy_mode = False
+    
+    if legacy_mode:
+        env.legacy_mode = True
+        env.virtualenv_path = f'{base_path}/venv'
+        env.special_requirements = special_requirements
+    return env
+
+
