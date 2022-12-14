2b0dff15d1ba583bb5f3e500db2c0acde933b2b1
Sascha Narr <saschanarr@gmail.com>
2022-12-14 13:15:20 +0100

    Fix OTP_TOTP_ISSUER setting

--- cosinnus/default_settings.py
+++ cosinnus/default_settings.py
@@ -792,7 +792,7 @@ def define_cosinnus_base_settings(project_settings, project_base_path):
     
     # django-otp settings
     # if set to None, will use the portal domain, which is fine
-    OTP_TOTP_ISSUER = None
+    OTP_TOTP_ISSUER = project_settings.get('OTP_TOTP_ISSUER', None)
     
     # django-simple captcha settings
     CAPTCHA_CHALLENGE_FUNCT = 'cosinnus.utils.captcha.dissimilar_random_char_challenge'
