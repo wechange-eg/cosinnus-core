b2b304a3e695d078675f29396978ffc7c35a7c8b
Sascha Sommer <6032754+saschasommer@users.noreply.github.com>
2023-09-20 08:24:04 +0200

    v3 navigation main content API extensions (#365)
    * Refactor remove_url_param into its own function
    
    * Add footer html, add random ids to sidebare MenuItems.
    
    * Add redoc return value example. Fix meta tags returning too much content

--- cosinnus/api_frontend/views/content.py
+++ cosinnus/api_frontend/views/content.py
@@ -5,6 +5,7 @@ from urllib.parse import urlparse, urlunparse
 import requests
 from bs4 import BeautifulSoup
 from django.http import QueryDict, Http404
+from django.utils.crypto import get_random_string
 from django.utils.translation import ugettext_lazy as _
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
@@ -20,6 +21,7 @@ from cosinnus.conf import settings
 from cosinnus.core.decorators.views import get_group_for_request
 from cosinnus.models import CosinnusPortal
 from cosinnus.models.user_dashboard import MenuItem, FONT_AWESOME_CLASS_FILTER
+from cosinnus.utils.http import remove_url_param
 
 # url prefixes that make the requested url be considered to belong in the "community" space
 V3_CONTENT_COMMUNITY_URL_PREFIXES = [
@@ -42,16 +44,50 @@ V3_CONTENT_BOTTOM_SIDEBAR_URL_SUFFIXES = [
 class MainContentView(APIView):
     """
     An endpoint that returns HTML content for any legacy view on the portal.
+    
+    Return values:
+      * `resolved_url`: the resolved URL after following all redirects the queried `url` parameter might have caused, and the actual URL from which the content HTML is delivered
+      * `status_code`: (int), HTTP status code of the response for the request sent to the `resolved_url`
+      * `content_html`: html from the `<div class="container">` frame to be inserted
+      * `footer_html`: html from the footer, to be inserted after the content html
+      * `head`: object with sub-entries for all content belonging in the head:
+        * `js_urls`: list of JS file URLs to be loaded in, basically all `<script src="..."` from the `<head>` tag of that page
+        * `css_urls`: list of CSS file URLs to be loaded in, basically all stylesheets from the `<head>` tag of that page
+        * `meta`: add all meta tags from the head
+        * `styles`: a list of strings of literal inline styles to be inserted before the HTML content is inserted
+      * `scripts`: a list of strings of literal inline JS script code to be executed before (after?) the HTML content is inserted
+      * `sub_navigation`: sidebar content, includes 3 lists: `"sub_navigation" {"top": [...], "middle": [...], "bottom": [...]}`
+        * middle is list of the apps that are enabled for the current space
+        * each list contains the usual menu items
+          * title (will be in current language)
+          * url link
+          * icon
+          * external (bool flag for `target="_blank"`)
+        * `sub_navigation` can be `null`, in that case the entire left sidebar is hidden
+      * `main_menu`: object with the following sub-entries, signifying how the main dropdown button looks like:
+        * `label`: title of the main menu dropdown
+        * `icon`: icon of the main menu dropdown, exclusive vs. `main_menu_image`
+        * `image`: image of the main menu dropdown, exclusive vs. `main_menu_icon`
+      * `announcements`: list of objects that contain data for the announcement banner, if one is active
+        * `[]` if None active, else a list `[{"text": "'"<str> Announcement text", "level": "<str> level-code"}, ...]`
+        * `level` can take the following values: "debug", "info", "success", "warning", "error". these levels are usually reflected in the color of the announcement background
     """
 
     renderer_classes = (CosinnusAPIFrontendJSONResponseRenderer, BrowsableAPIRenderer,)
     
+    # endpoint param
     url = None
+    
+    # set during processing
     group = None
+    has_leftnav = True  # can be toggled during HTML parsing to determine that this page has no leftnav
+    
+    # some return values
+    content_html = None
+    footer_html = None
     main_menu_label = None
     main_menu_icon = None
     main_menu_image = None
-    has_leftnav = True  # can be toggled during HTML parsing to determine that this page has no leftnav
     
     # todo: generate proper response, by either putting the entire response into a
     #       Serializer, or defining it by hand
@@ -71,7 +107,65 @@ class MainContentView(APIView):
             examples={
                 "application/json": {
                     "data": {
-                        "TODO": "DONT FORGET",
+                        "resolved_url": "/project/a-mein-bbb-projekt/?force_payment_popup=1",
+                        "status_code": 200,
+                        "content_html": "<div class=\"x-v3-container container\"> <div class=\"row app-main\"> ... </div></div>",
+                        "footer_html": "<div class=\"footer\"> ... </div>",
+                        "js_urls": [
+                            "/static/js/vendor/less.min.js",
+                            "/static/js/vendor/jquery-2.1.0.min.js",
+                        ],
+                        "css_urls": [
+                            "/static/css/select2.css",
+                            "/static/css/extra.css",
+                        ],
+                        "scripts": "var cosinnus_base_url = \"http://localhost:8000/\"; var cosinnus_active_group = \"a-mein-bbb-projekt\";  ...",
+                        "meta": "<meta charset=\"utf-8\"/><meta content=\"IE=edge\" http-equiv=\"X-UA-Compatible\"/><meta content=\"width=device-width, initial-scale=1\" name=\"viewport\"/> ...",
+                        "styles": ".my-contribution-badge {min-width: 50px;border-radius: 20px;color: #FFF;font-size: 12px;padding: 2px 6px; margin-left: 5px;}.my-contribution-badge.red {background-color: rgb(245, 85, 0);} ...",
+                        "sub_navigation": {
+                            "top": [
+                                {
+                                    "id": "Sidebar-pws2dgLA",
+                                    "icon": "fa-lightbulb-o",
+                                    "label": "Microsite",
+                                    "url": "/project/a-mein-bbb-projekt/microsite/",
+                                    "is_external": FONT_AWESOME_CLASS_FILTER,
+                                    "image": None,
+                                    "badge": None
+                                }
+                            ],
+                            "middle": [
+                                {
+                                    "id": "Sidebar-FTZD61ZZ",
+                                    "icon": "fa-th-large",
+                                    "label": "Projektdashboard",
+                                    "url": "/project/a-mein-bbb-projekt/?browse=true",
+                                    "is_external": False,
+                                    "image": None,
+                                    "badge": None
+                                },
+                            ],
+                            "bottom": [
+                                {
+                                    "id": "Sidebar-6vcO7aYp",
+                                    "icon": "fa-cogs",
+                                    "label": "Einstellungen",
+                                    "url": "/project/a-mein-bbb-projekt/edit/",
+                                    "is_external": False,
+                                    "image": None,
+                                    "badge": None
+                                },
+                            ]
+                        },
+                        "main_menu": {
+                            "label": "A Mein BBB Projekt",
+                            "icon": None,
+                            "image": "/media/cosinnus_portals/portal_saschas_local_dev/avatars/group/xj1VxzF3viA4.jpg"
+                        },
+                        "announcements": [{
+                            "text": "This is an example announcement",
+                            "level": "warning"
+                        }],
                     },
                     "version": COSINNUS_VERSION,
                     "timestamp": 1658414865.057476
@@ -81,6 +175,7 @@ class MainContentView(APIView):
     )
     def get(self, request):
         self.url = request.query_params.get('url', '').strip()
+        self.url = remove_url_param(self.url, 'v', '3')
         django_request = copy(request._request)
         if not self.url:
             raise ValidationError('Missing required parameter: url')
@@ -101,22 +196,26 @@ class MainContentView(APIView):
         self._resolve_request_group(resolved_url, django_request)
         # parse the response's html
         html = self._clean_response_html(str(response.text))
+        self._parse_html_content(html)
         
         # TODO compress html? or do server-side request compression?
         
         data = {
             "resolved_url": resolved_url,
             "status_code": response.status_code,
-            "content_html": self._parse_content_html(html),
+            "content_html": self.content_html,
+            "footer_html": self.footer_html,
             "js_urls": self._parse_js_urls(html),
             "css_urls": self._parse_css_urls(html),
             "scripts": self._parse_inline_tag_contents(html, 'script'),
             "meta": self._parse_tags(html, 'meta'),
             "styles": self._parse_inline_tag_contents(html, 'style'),
             "sub_navigation": self._parse_leftnav_menu(html),  # can be None if no left navigation should be shown
-            "main_menu_label": self.main_menu_label,  # either <name of group>, "personal space", or "community"
-            "main_menu_icon": self.main_menu_icon,  # exclusive with `main_menu_image`, only one can be non-None!
-            "main_menu_image": self.main_menu_image,  # exclusive with `main_menu_icon`, only one can be non-None!
+            "main_menu": {
+                "label": self.main_menu_label,  # either <name of group>, "personal space", or "community"
+                "icon": self.main_menu_icon,  # exclusive with `main_menu_image`, only one can be non-None!
+                "image": self.main_menu_image,  # exclusive with `main_menu_icon`, only one can be non-None!
+            },
             "announcements": self._get_announcements(),
         }
         
@@ -137,24 +236,6 @@ class MainContentView(APIView):
         session = requests.Session()
         session.headers.update(dict(request.headers))
         session.cookies.update(request.COOKIES)
-        
-        # TODO refactor this v=3 param deletion!
-        parsed = urlparse(url)
-        query = parsed.query
-        dic = QueryDict(query)
-        dic._mutable = True
-        if dic.get('v', None) == '3':
-            del dic['v']
-            query = dic.urlencode()
-        url = urlunparse(
-            (parsed.scheme,
-            parsed.netloc,
-            parsed.path,
-            parsed.params,
-            query,
-            parsed.fragment)
-        )
-        
         response = session.get(url)
         for history_response in response.history:
             response.cookies.update(history_response.cookies)
@@ -213,15 +294,15 @@ class MainContentView(APIView):
             self.main_menu_icon = 'fa-sitemap'  # TODO: icon for Community space?
         else:
             self.main_menu_label = _('Personal')
-            self.main_menu_icon = 'fa-user'  # TODO: icon for Personal space?
+            self.main_menu_icon = 'fa-user'  # TODO: icon for Personal space? use user avatar if they have one instead?
         
-    def _parse_content_html(self, html):
-        """ Parses only the content frame of the full html that can be put into any other part of the site.
-            May do some reformatting for full-page views to transform the body tag into a div. """
-        # TODO: resolve view to check view parameters if this is a full content or inner content view!
+    def _parse_html_content(self, html):
+        """ Parses the content frame of the full html that can be put into any other part of the site.
+            May do some reformatting for full-page views to transform the body tag into a div.
+            Also parses the footer html content. """
         soup = BeautifulSoup(html, 'html.parser')
         # remove all tags we never want to see in the content, like inline scripts and styles
-        for bad_tag_literal in ['nav', 'scripst', 'style']:
+        for bad_tag_literal in ['nav', 'script', 'style']:
             for bad_tag in soup.find_all(bad_tag_literal):
                 bad_tag.decompose()
         # try to extract our page's inner container, below the breadcrumb
@@ -234,7 +315,10 @@ class MainContentView(APIView):
             content = soup.find('body')
             content = content.decode_contents()
             self.has_leftnav = False
-        return str(content or '').strip()
+        self.content_html = str(content or '').strip()
+        footer = soup.find('div', class_='x-v3-footer')
+        if footer:
+            self.footer_html = str(footer.decode_contents()).strip()
     
     def _extract_fa_icon(self, tag):
         """ Extracts the actual font-awesome icon class name from the first i tag within the given tag tree """
@@ -271,6 +355,7 @@ class MainContentView(APIView):
                 leftnav_link.text.strip(),
                 href,
                 icon=self._extract_fa_icon(leftnav_link),  # TODO. filter/map-convert these icons to frontend icons?
+                id='Sidebar-' + get_random_string(8),
             )
             # select the proper subnav for this menu to go to, by type of URL
             target_subnav = middle
