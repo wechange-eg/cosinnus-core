a7e728a0eca034ab377098078b150a7aaa75b1b2
Alexej Starschenko <alexej@2e2a.de>
2023-10-09 13:21:55 +0200

    Add temporary data model

---
+++ cosinnus/models/storage.py
@@ -0,0 +1,19 @@
+import logging
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+logger = logging.getLogger('cosinnus')
+
+
+class TemporaryData(models.Model):
+    created = models.DateTimeField(verbose_name=_('Created'), editable=False, auto_now_add=True)
+    deletion_after = models.DateTimeField(verbose_name=_('Deletion after'))
+    description = models.CharField(max_length=255, verbose_name=_('Description'))
+    data = models.BinaryField(verbose_name=_('Data'))
+
+    class Meta:
+        verbose_name = _('Temporary Data')
+        verbose_name_plural = _('Temporary Data')
+        ordering = ['-created']
