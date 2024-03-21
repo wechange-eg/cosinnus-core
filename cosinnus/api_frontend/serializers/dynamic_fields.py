93f693d53afc0a5ad1c5279eea9b36d5949aa48a
Sascha Sommer <6032754+saschasommer@users.noreply.github.com>
2024-03-21 14:20:36 +0100

    Revert "Reverts from redesign-dev for we-020 RC (#430)"
    This reverts commit 4fea6a55e5b784175ce81531f90e5315e4f0e9f3.

--- cosinnus/api_frontend/serializers/dynamic_fields.py
+++ cosinnus/api_frontend/serializers/dynamic_fields.py
@@ -5,6 +5,8 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework.fields import get_error_detail, empty as drf_empty
 
+from cosinnus.conf import settings
+from cosinnus.dynamic_fields import dynamic_fields
 from cosinnus.dynamic_fields.dynamic_formfields import EXTRA_FIELD_TYPE_FORMFIELD_GENERATORS
 
 
@@ -33,8 +35,9 @@ class CosinnusUserDynamicFieldsSerializerMixin(object):
         for field_name, field_options in self.DYNAMIC_FIELD_SETTINGS.items():
             if self.filter_included_fields_by_option_name and not getattr(field_options, self.filter_included_fields_by_option_name, False):
                 continue
-            # all dynamic fields are of type (drf serializer) CharField, validation will take place manually
+            # dynamic fields are of type (drf serializer) CharField, BooleanField or IntergerField, validation will take place manually
             if field_options.multiple:
+                # All lists are serialized as CharField lists.
                 field = serializers.ListField(
                     child=serializers.CharField(
                         required=field_options.required if not self.all_fields_optional else False,
@@ -49,16 +52,42 @@ class CosinnusUserDynamicFieldsSerializerMixin(object):
                     help_text=f'This is a dynamic field of data type: <List>({field_options.type})'
                 )
             else:
-                field = serializers.CharField(
-                    required=field_options.required if not self.all_fields_optional else False,
-                    allow_blank=not field_options.required if self.all_fields_optional else True,
-                    allow_null=not field_options.required if self.all_fields_optional else True,
-                    default=drf_empty if field_options.required else field_options.default,
-                    source=f'cosinnus_profile.dynamic_fields.{field_name}',
-                    help_text=f'This is a dynamic field of data type: {field_options.type}'
-                )
+                # Serialize as CharField, BooleanField or IntegerField depending on the field type.
+                default_serializer = serializers.CharField
+                field_serializer_map = {
+                    dynamic_fields.DYNAMIC_FIELD_TYPE_BOOLEAN: serializers.BooleanField,
+                    dynamic_fields.DYNAMIC_FIELD_TYPE_INT: serializers.IntegerField,
+                }
+                field_serializer = field_serializer_map.get(field_options.type, default_serializer)
+                serializer_params = {
+                    'required': field_options.required if not self.all_fields_optional else False,
+                    'allow_null': not field_options.required if self.all_fields_optional else True,
+                    'default': drf_empty if field_options.required else field_options.default,
+                    'source': f'cosinnus_profile.dynamic_fields.{field_name}',
+                    'help_text': f'This is a dynamic field of data type: {field_options.type}'
+                }
+                if field_serializer == serializers.CharField:
+                    serializer_params.update({
+                        'allow_blank': not field_options.required if self.all_fields_optional else True,
+                    })
+                field = field_serializer(**serializer_params)
             setattr(self, field_name, field)
             self._declared_fields[field_name] = field
+
+            # Add serializer for translated dynamic fields
+            if (settings.COSINNUS_TRANSLATED_FIELDS_ENABLED
+                    and field_name in settings.COSINNUS_USERPROFILE_EXTRA_FIELDS_TRANSLATED_FIELDS):
+                for language_code, __ in settings.LANGUAGES:
+                    translated_field_name = f'{field_name}__{language_code}'
+                    # Using a profile function added by the TranslateableFieldsModelMixin as source.
+                    source = f'cosinnus_profile.get_{translated_field_name}'
+                    field = serializers.CharField(
+                        required=False, allow_null=True, allow_blank=True, source=source,
+                        help_text=f'This is a dynamic field translation for {field_name}.',
+                    )
+                    setattr(self, translated_field_name, field)
+                    self._declared_fields[translated_field_name] = field
+
         super().__init__(*args, **kwargs)
     
     def validate(self, attrs):
@@ -114,8 +143,30 @@ class CosinnusUserDynamicFieldsSerializerMixin(object):
             field_value = dynamic_field_attr_dict.get(field_name, '')
             userprofile.dynamic_fields[field_name] = field_value
             profile_changed = True
-            
+
+        # Save dynamic field translations
+        if userprofile.has_dynamic_field_translations:
+            translations_changed = False
+            dynamic_field_translations = userprofile.translations.get('dynamic_fields', {})
+            # Note: translations are generated from the profile method and thus not stored under "dynamic_fields" in the
+            # validated data.
+            profile_attr_dict = validated_data.get('cosinnus_profile', {})
+            for field_name in userprofile.translatable_dynamic_fields:
+                field_options = self.DYNAMIC_FIELD_SETTINGS.get('field_name')
+                if self.filter_included_fields_by_option_name and not getattr(field_options, self.filter_included_fields_by_option_name, False):
+                    continue
+                for language_code, __ in settings.LANGUAGES:
+                    translated_field_name = f'get_{field_name}__{language_code}'
+                    if translated_field_name in profile_attr_dict:
+                        translated_field_value = profile_attr_dict.get(translated_field_name)
+                        if language_code not in dynamic_field_translations:
+                            dynamic_field_translations[language_code] = {}
+                        dynamic_field_translations[language_code][field_name] = translated_field_value
+                        translations_changed = True
+            if translations_changed:
+                profile_changed = True
+                userprofile.translations['dynamic_fields'] = dynamic_field_translations
+
         if profile_changed and save:
             userprofile.save()
-        
-    
+
