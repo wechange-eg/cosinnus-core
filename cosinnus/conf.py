3408e15b2d07c682294242f744e9bdb5fa08ec17
Sascha Sommer <saschanarr@gmail.com>
2024-04-18 12:32:01 +0200

    Nextcloud config: Remove onlyoffice from sync-command cloud apps

--- cosinnus/conf.py
+++ cosinnus/conf.py
@@ -1164,7 +1164,7 @@ class CosinnusConf(AppConf):
         'ALLOW_PUBLIC_UPLOADS': 'no', # "yes" or "no"
         'ALLOW_AUTOCOMPLETE_USERS': 'no', # "yes" or "no"
         'SEND_EMAIL_TO_NEW_USERS': 'no', # "yes" or "no"
-        'ENABLE_APP_IDS': ["groupfolders", "onlyoffice", "sociallogin", "cspworkaround", "firstrunwizard", "fulltextsearch_admin-api"], # list of string app ids
+        'ENABLE_APP_IDS': ["groupfolders", "sociallogin", "cspworkaround", "firstrunwizard", "fulltextsearch_admin-api"], # list of string app ids
         'DISABLE_APP_IDS': ["photos", "activity", "systemtags", "dashboard"], # list of string app ids
     }
     
