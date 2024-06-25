3f7cd98c1a836b47add53ba28b53d6ff5ddf225a
Sascha Sommer <saschanarr@gmail.com>
2024-06-25 13:45:59 +0200

    v3 navigation api: Add "Calendar" icon link on portals with Events group
    Added to "services" on right hand side.

--- cosinnus/api_frontend/views/navigation.py
+++ cosinnus/api_frontend/views/navigation.py
@@ -29,6 +29,7 @@ from cosinnus.utils.permissions import (
     check_user_can_create_groups,
     check_user_portal_manager,
 )
+from cosinnus.utils.urls import group_aware_reverse
 from cosinnus.utils.user import get_unread_message_count_for_user
 from cosinnus.utils.version_history import get_version_history_for_user, mark_version_history_as_read
 from cosinnus.views.user_dashboard import MyGroupsClusteredMixin
@@ -1275,7 +1276,15 @@ class MainNavigationView(LanguageMenuItemMixin, APIView):
                     services_navigation_items.append(
                         MenuItem(_('Messages'), reverse('postman:inbox'), icon='messages', id='Messages')
                     )
-
+        # add "Calendar" link to services for all logged in users if the portal has a seperate Events forum group
+        if request.user.is_authenticated:
+            if settings.NEWW_EVENTS_GROUP_SLUG and settings.NEWW_EVENTS_GROUP_SLUG != settings.NEWW_FORUM_GROUP_SLUG:
+                events_url = group_aware_reverse(
+                    'cosinnus:event:list', kwargs={'group': settings.NEWW_EVENTS_GROUP_SLUG}
+                )
+                services_navigation_items.insert(
+                    0, MenuItem(_('Events'), events_url, icon='fa-calendar', is_external=False, id='Events')
+                )
         # add "Discover" link to services for all logged in users and additionally for non-logged-in users on open
         # portals
         if not settings.COSINNUS_USER_EXTERNAL_USERS_FORBIDDEN or (
