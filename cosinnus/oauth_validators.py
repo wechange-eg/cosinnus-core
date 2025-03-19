3a0aef3c0582433046278c7844b4e8115ac129d0
Alexej Starschenko <alexej@2e2a.de>
2025-03-19 14:20:15 +0100

    Add group memberships and urls to OpenId userinfo endpoint

--- cosinnus/oauth_validators.py
+++ cosinnus/oauth_validators.py
@@ -1,6 +1,7 @@
 from oauth2_provider.oauth2_validators import OAuth2Validator
 
-from cosinnus.models.group import CosinnusGroup, CosinnusPortal
+from cosinnus.models.group import CosinnusPortal
+from cosinnus.models.membership import MEMBERSHIP_ADMIN, MEMBERSHIP_MANAGER, MEMBERSHIP_MEMBER
 
 
 class CustomOAuth2Validator(OAuth2Validator):
@@ -10,7 +11,7 @@ class CustomOAuth2Validator(OAuth2Validator):
         """
         Populate userinfo as returned by the /o/userinfo/ endpoint.
         See https://django-oauth-toolkit.readthedocs.io/en/latest/oidc.html#adding-information-to-the-userinfo-service
-        Note: Mostly a copy of the cosinnus_cloud.views.OAuthView
+        Note: Mostly a copy of the cosinnus_cloud.views.OAuthView and cosinnus.api.views.user.OAuthUserView
         """
         claims = super().get_userinfo_claims(request)
         user = request.user
@@ -25,12 +26,37 @@ class CustomOAuth2Validator(OAuth2Validator):
             if avatar_url:
                 avatar_url = portal.get_absolute_url() + avatar_url
 
+            membership_type_map = {
+                MEMBERSHIP_ADMIN: 'admins',
+                MEMBERSHIP_MANAGER: 'managers',
+                MEMBERSHIP_MEMBER: 'users',
+            }
+            group_dict = {}
+            group_url_dict = {}
+            for group in user.cosinnus_profile.cosinnus_groups:
+                # add a {group_id --> membership type} entry for each group the user is a member of
+                membership = group.memberships.filter(user=user)[0]
+                membership_type = membership_type_map.get(membership.status, None)
+                if membership_type:
+                    group_dict.update(
+                        {
+                            str(group.id): membership_type,
+                        }
+                    )
+                # add a {group_id --> group_url} entry for each group the user is a member of
+                group_url_dict.update(
+                    {
+                        str(group.id): group.get_absolute_url(),
+                    }
+                )
+
             claims.update(
                 {
                     'email': email,
                     'name': user.cosinnus_profile.get_external_full_name(),
                     'avatar': avatar_url,
-                    'groups': [group.name for group in CosinnusGroup.objects.get_for_user(user)],
+                    'group': group_dict,
+                    'group_urls': group_url_dict,
                 }
             )
         return claims
