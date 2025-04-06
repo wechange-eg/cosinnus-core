2829f03fede08ae933692220d21a49c4b3d11688
Sascha Sommer <saschanarr@gmail.com>
2025-04-06 16:37:52 +0200

    cooperation #478: Fix v2 api issues, add swagger docs.
    - Add `id` field to group serializer
    - Add `tags` field to note serializer
    - No longer allow any non-serializer field names as filter params for v2 endpoints
    - Filter the config endpoint to only return COSINNUS settings
    - Document the query param 'group_id' for note and event serializer
    - Document the purpose of some group/project v2 endpoints

--- cosinnus_note/api/views.py
+++ cosinnus_note/api/views.py
@@ -1,13 +1,39 @@
-from rest_framework import viewsets
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import serializers, viewsets
 
 from cosinnus.api.views.mixins import CosinnusFilterQuerySetMixin, PublicTaggableObjectFilterMixin
 from cosinnus_note.api.serializers import NoteListSerializer, NoteRetrieveSerializer
 from cosinnus_note.models import Note
 
 
+class MyQueryParamSerializer(serializers.Serializer):
+    my_name = serializers.CharField()
+    my_number = serializers.IntegerField(help_text='Some custom description for the number')
+    my_bool = serializers.BooleanField()
+
+
 class NoteViewSet(CosinnusFilterQuerySetMixin, PublicTaggableObjectFilterMixin, viewsets.ReadOnlyModelViewSet):
+    """
+    An endpoint that returns publicly visible news posts.
+    """
+
     queryset = Note.objects.all()
     MANAGED_TAGS_FILTER_ON_GROUP = True
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                'group_id',
+                openapi.IN_QUERY,
+                description='If provided, will only return items belonging to the group/project with the given '
+                'group id. Group ids are displayed in the `/groups/` and `/projects/` endpoints.',
+                type=openapi.TYPE_INTEGER,
+            ),
+        ],
+    )
+    def list(self, request, **kwargs):
+        return super().list(request, **kwargs)
 
     def get_serializer_class(self):
         if self.action == 'list':
