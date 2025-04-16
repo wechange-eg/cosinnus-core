6bb55a613034e19725ceac27612bf783bdaf3ad6
Alexej Starschenko <alexej@2e2a.de>
2025-04-16 15:15:35 +0200

    Remove cloud from quicksearch

--- cosinnus/conf.py
+++ cosinnus/conf.py
@@ -1213,9 +1213,6 @@ class CosinnusConf(AppConf):
     CLOUD_ENABLED = False
     # whether to show the cosinnus cloud dashboard widget
     CLOUD_DASHBOARD_WIDGET_ENABLED = True
-    # whether the quicksearch includes cloud results.
-    # comes with a large reduction in search speed as nextcloud is slow
-    CLOUD_QUICKSEARCH_ENABLED = False
 
     # base url of the nextcloud service, without trailing slash
     CLOUD_NEXTCLOUD_URL = None
