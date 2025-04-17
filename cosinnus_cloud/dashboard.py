e7894c0048003379a5f236c7984006d37bca7cb8
Alexej Starschenko <alexej@2e2a.de>
2025-04-17 14:45:59 +0200

    Stub group dashboard cloud widget

--- cosinnus_cloud/dashboard.py
+++ cosinnus_cloud/dashboard.py
@@ -5,17 +5,11 @@ import logging
 
 from django import forms
 from django.template.loader import render_to_string
-from django.utils.encoding import force_str
 from django.utils.translation import gettext_lazy as _
 
-from cosinnus.conf import settings
 from cosinnus.models.widget import WidgetConfig
 from cosinnus.utils.dashboard import DashboardWidget, DashboardWidgetForm
 from cosinnus.utils.urls import group_aware_reverse
-from cosinnus_cloud.hooks import get_nc_user_id
-from cosinnus_cloud.models import CloudFile
-from cosinnus_cloud.utils import nextcloud
-from cosinnus_cloud.utils.cosinnus import is_cloud_enabled_for_group
 
 logger = logging.getLogger('cosinnus')
 
@@ -35,48 +29,12 @@ class Latest(DashboardWidget):
     def get_data(self, offset=0):
         """Returns a tuple (data, rows_returned, has_more) of the rendered data and how many items were returned.
         if has_more == False, the receiving widget will assume no further data can be loaded.
+        Disabled/Stubbed as the respective NextCloud API is not available anymore.
         """
-        count = int(self.config['amount'])
-
         rows = []
         total_count = 0
         has_more = False
         had_error = False
-        if (
-            is_cloud_enabled_for_group(self.config.group)
-            and self.config.group.nextcloud_group_id
-            and self.config.group.nextcloud_groupfolder_name
-        ):
-            try:
-                response = nextcloud.find_newest_files(
-                    userid=get_nc_user_id(self.request.user),
-                    folder=self.config.group.nextcloud_groupfolder_name,
-                    page_size=count,
-                    page=offset / count + 1,
-                )
-            except Exception as e:
-                logger.info(
-                    'An error occured during Nextcloud widget data retrieval (dashboard.py)! Exception in extra.',
-                    extra={'exc_str': force_str(e), 'exception': e},
-                )
-                had_error = True
-
-            if had_error:
-                total_count = ' '
-                rows = []
-            else:
-                total_count = response['meta']['total']
-                has_more = offset + len(rows) < total_count
-                rows = [
-                    CloudFile(
-                        title=doc['info']['file'],
-                        path=doc['info']['path'],
-                        folder=doc['info']['dir'],
-                        url=f"{settings.COSINNUS_CLOUD_NEXTCLOUD_URL}{doc['link']}",
-                        download_url=f"{settings.COSINNUS_CLOUD_NEXTCLOUD_URL}/remote.php/webdav{doc['info']['path']}",
-                    )
-                    for doc in response['documents']
-                ]
 
         data = {
             'rows': rows,
