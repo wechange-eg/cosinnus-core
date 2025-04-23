c1b711760af59886963814abd4b4f8b8c092de44
Alexej Starschenko <alexej@2e2a.de>
2025-04-23 14:07:15 +0200

    Improve online / on-site filter
    - Automatically add 'online_or_onsite' filter to hidden filters if DIFFERENTIATE_ONLINE_AND_ONSITE_EVENTS is disabled
    - Make online_or_onsite filtering DRYer
    
    Fixup for: https://git.wechange.de/gl/code/portals/boell/-/issues/59

--- cosinnus_event/filters.py
+++ cosinnus_event/filters.py
@@ -57,20 +57,27 @@ class EventFilter(CosinnusFilterSet):
         model = Event
         fields = ['creator', 'o', 'online_or_onsite']
 
+    def __init__(self, *args, **kwargs):
+        # init hidden filters according to disabled features
+        if (
+            not settings.COSINNUS_EVENT_DIFFERENTIATE_ONLINE_AND_ONSITE_EVENTS
+            and 'online_or_onsite' not in self.hidden_filters
+        ):
+            self.hidden_filters.append('online_or_onsite')
+        super(EventFilter, self).__init__(*args, **kwargs)
+
     def filter_online_or_onsite(self, queryset, name, value):
         """Filter queryset by online (has no location) or on-site (has location).
         Note: Filtering non-proxy-events by media_tag location and proxy events by the group/conference location.
         """
+        online_filter = (
+            Q(is_hidden_group_proxy=False, media_tag__location__isnull=True)
+            | Q(is_hidden_group_proxy=False, media_tag__location='')
+            | Q(is_hidden_group_proxy=True, group__locations__location__isnull=True)
+            | Q(is_hidden_group_proxy=True, group__locations__location='')
+        )
         if value == self.ONLINE_CHOICE:
-            queryset = queryset.filter(
-                Q(is_hidden_group_proxy=False, media_tag__location__isnull=True)
-                | Q(is_hidden_group_proxy=False, media_tag__location='')
-                | Q(is_hidden_group_proxy=True, group__locations__location__isnull=True)
-                | Q(is_hidden_group_proxy=True, group__locations__location='')
-            )
+            queryset = queryset.filter(online_filter)
         elif value == self.ON_SITE_CHOICE:
-            queryset = queryset.exclude(is_hidden_group_proxy=False, media_tag__location__isnull=True)
-            queryset = queryset.exclude(is_hidden_group_proxy=False, media_tag__location='')
-            queryset = queryset.exclude(is_hidden_group_proxy=True, group__locations__location__isnull=True)
-            queryset = queryset.exclude(is_hidden_group_proxy=True, group__locations__location='')
+            queryset = queryset.exclude(online_filter)
         return queryset
