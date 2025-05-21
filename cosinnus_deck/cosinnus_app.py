9e051993bfcad9d5510a507fa4e637c37521896a
Alexej Starschenko <alexej@2e2a.de>
2025-05-21 15:23:08 +0200

    Add COSINNUS_DECK_ENABLED setting

--- cosinnus_deck/cosinnus_app.py
+++ cosinnus_deck/cosinnus_app.py
@@ -9,6 +9,8 @@ from cosinnus.conf import settings
 def register():
     if 'cosinnus_deck' in getattr(settings, 'COSINNUS_DISABLED_COSINNUS_APPS', []):
         return
+    if not getattr(settings, 'COSINNUS_DECK_ENABLED', False):
+        return
     if not getattr(settings, 'COSINNUS_CLOUD_ENABLED', False):
         raise ImproperlyConfigured('COSINNUS_CLOUD_ENABLED must be True if the cosinnus_deck app is enabled.')
 
