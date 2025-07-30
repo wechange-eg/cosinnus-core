13fe95dd39f8117c975e0ef2e305afef663326ea
Alexej Starschenko <alexej@2e2a.de>
2025-07-30 11:02:01 +0200

    Deck integration fixups
    - add scope to js
    - remove handling of uninitialized boards, as it will be done in the frontend
    - use white content in todo migration template
    - fix typos and wording

--- cosinnus_deck/views.py
+++ cosinnus_deck/views.py
@@ -1,14 +1,11 @@
 import json
 
-from django.contrib import messages
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
-from django.utils.translation import gettext_lazy as _
 from django.views.generic.base import TemplateView, View
 
 from cosinnus.models.group import CosinnusGroup, CosinnusPortal
 from cosinnus.utils.group import get_cosinnus_group_model
-from cosinnus.utils.permissions import check_ug_admin
 from cosinnus.views.mixins.group import RequireLoggedInMixin, RequireReadMixin, RequireWriteGrouplessMixin
 from cosinnus_todo.models import TodoEntry
 
@@ -17,17 +14,6 @@ class DeckView(RequireReadMixin, TemplateView):
     """Main deck app view containing a div used for the frontend app initialization."""
 
     template_name = 'cosinnus_deck/deck.html'
-
-    def get(self, request, *args, **kwargs):
-        if not self.group.nextcloud_deck_board_id and check_ug_admin(request.user, self.group):
-            # add admin warning
-            message = _(
-                'If the task-board is not available withing a few minutes, some technical difficulties occurred with '
-                'the board service. Try disabling and re-enabling the task-board app in the settings. '
-                'If the problems persist, please contact the support. We apologize for the inconveniences!'
-            )
-            messages.warning(self.request, message)
-        return super(DeckView, self).get(request, *args, **kwargs)
 
 
 deck_view = DeckView.as_view()
