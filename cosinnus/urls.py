b7f4bb2aa62fdaeff72f0bbdd8602f46bb213c5f
Jens Möller <jensmoeller.git@gmail.com>
2025-10-10 21:38:32 +0200

    itvsh#25: add feature model_export combining user_export and group_export
    - refactor user_export feature to model_export
    - add group_export class
    - make view export-model generic
    - abstract model processing logic into abstract base class
    
    note:
    this supersedes the existing user_export, which could be removed separately
    
    https://git.wechange.de/gl/code/portals/itvsh/-/issues/25

--- cosinnus/urls.py
+++ cosinnus/urls.py
@@ -46,6 +46,7 @@ from cosinnus.views import (
     map_api,
     microsite,
     mitwirkomat,
+    model_export,
     profile,
     search,
     statistics,
@@ -380,6 +381,26 @@ if getattr(settings, 'COSINNUS_USER_EXPORT_ADMINISTRATION_VIEWS_ENABLED', False)
             'administration/user_export/download/xlsx/',
             user_export.user_export_xlsx_download_view,
             name='administration-user-export-xlsx-download',
+        ),
+    ]
+
+
+if getattr(settings, 'COSINNUS_MODEL_EXPORT_ADMINISTRATION_VIEWS_ENABLED', False):
+    urlpatterns += [
+        path(
+            'administration/model_export/<str:slug>/',
+            model_export.model_export_view,
+            name='administration-model-export',
+        ),
+        path(
+            'administration/model_export/<str:slug>/download/csv/',
+            model_export.model_export_csv_download_view,
+            name='administration-model-export-csv-download',
+        ),
+        path(
+            'administration/model_export/<str:slug>/download/xlsx/',
+            model_export.model_export_xlsx_download_view,
+            name='administration-model-export-xlsx-download',
         ),
     ]
 
