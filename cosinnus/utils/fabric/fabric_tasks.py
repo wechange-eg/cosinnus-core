wechange-eg/cosinnus-core

commit 387bbf431919e8231692dcd7c0cc514c720d8128

fabric_tasks: fix enablegitremoteoncore
 to instantly set the correct core branch after a 'fulldeploy'

cosinnus/utils/fabric/fabric_tasks.py
@@ -135,6 +135,9 @@ def enablegitremoteoncore(_ctx):
     with c.cd(env.path):
         with c.cd(f'{env.cosinnus_src_path}'):
             c.run('git config --local --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/*')
+            c.run('git fetch')
+            c.run(f'git checkout {env.pull_branch}')
+            c.run('git pull')
 
 
 @task

commit 356bbb53ef8dd38b10419978e6d24beb2b2dbec0

Fabfile: Fix wrong cosinnus-core branch checkout after fulldeploy
Introduced by commit 387bbf431919e8231692dcd7c0cc514c720d8128 "fabric_tasks: fix enablegitremoteoncore"

cosinnus/utils/fabric/fabric_tasks.py
@@ -136,7 +136,7 @@ def enablegitremoteoncore(_ctx):
         with c.cd(f'{env.cosinnus_src_path}'):
             c.run('git config --local --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/*')
             c.run('git fetch')
-            c.run(f'git checkout {env.pull_branch}')
+            c.run(f'git checkout {env.cosinnus_pull_branch}')
             c.run('git pull')
 
 