6606248a2270dd45846e174638817a96129a87d8
Alexej Starschenko <alexej@2e2a.de>
2025-12-04 10:40:23 +0100

    Add location and topics to events API

--- cosinnus_event/calendar/serializers.py
+++ cosinnus_event/calendar/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
+from cosinnus.api_frontend.serializers.media_tag import CosinnusMediaTagSerializerMixin
 from cosinnus.models import BaseTagObject
+from cosinnus.models.tagged import get_tag_object_model
 from cosinnus_event.models import Event
 
 
@@ -34,11 +36,44 @@ class CalendarPublicEventListSerializer(serializers.ModelSerializer):
         )
 
 
-class CalendarPublicEventSerializer(serializers.ModelSerializer):
+class CalendarPublicEventSerializer(CosinnusMediaTagSerializerMixin, serializers.ModelSerializer):
     """Complete Serializer for events in the calendar API."""
 
     # TODO: prefetch media_tag when adding location & topics
     description = serializers.CharField(source='note')
+    topics = serializers.MultipleChoiceField(
+        source='media_tag.get_topic_ids',
+        required=False,
+        allow_blank=True,
+        default=list,
+        choices=get_tag_object_model().TOPIC_CHOICES,
+        help_text=f'Array of ints for corresponding topics: {str(get_tag_object_model().TOPIC_CHOICES)}',
+    )
+    location = serializers.CharField(
+        source='media_tag.location',
+        required=False,
+        allow_blank=True,
+        default=None,
+        help_text='On input, this string is used to determine the lat/lon fields using a nominatim service',
+    )
+    location_lat = serializers.FloatField(
+        source='media_tag.location_lat',
+        required=False,
+        default=None,
+        help_text=(
+            'If not supplied, but `location` is supplied, this will be determined automatically via nominatim from the '
+            'string in `location`. If supplied, will only be saved if `location` is also supplied.'
+        ),
+    )
+    location_lon = serializers.FloatField(
+        source='media_tag.location_lon',
+        required=False,
+        default=None,
+        help_text=(
+            'If not supplied, but `location` is supplied, this will be determined automatically via nominatim from the '
+            'string in `location`. If supplied, will only be saved if `location` is also supplied.'
+        ),
+    )
 
     class Meta:
         model = Event
@@ -49,11 +84,15 @@ class CalendarPublicEventSerializer(serializers.ModelSerializer):
             'to_date',
             'description',
             'image',
-            #'topics',
-            #'tags',
+            'topics',
+            'location',
+            'location_lat',
+            'location_lon',
         )
 
     def create(self, validated_data):
+        # get nested media tag data
+        media_tag_data = validated_data.pop('media_tag', {})
         # set group and creator
         validated_data.update(
             {
@@ -61,8 +100,22 @@ class CalendarPublicEventSerializer(serializers.ModelSerializer):
                 'creator': self.context['request'].user,
             }
         )
-        event = super().create(validated_data)
+        instance = Event.objects.create(**validated_data)
         # set event visibility to public
-        event.media_tag.visibility = BaseTagObject.VISIBILITY_ALL
-        event.media_tag.save()
-        return event
+        instance.media_tag.visibility = BaseTagObject.VISIBILITY_ALL
+        # save media tag fields
+        if media_tag_data:
+            self.save_media_tag(instance.media_tag, media_tag_data)
+        return instance
+
+    def update(self, instance, validated_data):
+        # get nested media tag data
+        media_tag_data = validated_data.pop('media_tag', {})
+        # update event fields
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
+        instance.save()
+        # update media tag
+        if media_tag_data:
+            self.save_media_tag(instance.media_tag, media_tag_data)
+        return instance
