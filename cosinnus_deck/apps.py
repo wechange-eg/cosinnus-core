760252aa6df2e66ba85e2765fed8b185f82ddfd9
Alexej Starschenko <alexej@2e2a.de>
2025-12-11 15:09:02 +0100

    Refactor app registration
    - Move integration handler initialization to cosinnus_app register to avoid duplicate checks for disabled app.
    - Move deck settings dependency verification to checks.

--- cosinnus_deck/apps.py
+++ cosinnus_deck/apps.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.apps import AppConfig
-from django.conf import settings
 
 
 class CosinnusDeckAppConfig(AppConfig):
@@ -9,11 +8,6 @@ class CosinnusDeckAppConfig(AppConfig):
 
     def ready(self):
         from cosinnus_deck import cosinnus_app
-        from cosinnus_deck.integration import DeckIntegrationHandler
 
         # register app
         cosinnus_app.register()
-
-        if settings.COSINNUS_DECK_ENABLED:
-            # initialize integration handler
-            DeckIntegrationHandler(app_name=self.name)
