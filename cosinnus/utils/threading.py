501de02db192f5ed44d841c39e274949656c9bf0
Jens Möller <jensmoeller.git@gmail.com>
2025-12-13 15:42:12 +0100

    new setting `USE_WORKER_THREADS` to disable worker threads entirely
    - if not True, use vanilla Thread

--- cosinnus/utils/threading.py
+++ cosinnus/utils/threading.py
@@ -4,35 +4,42 @@ from functools import wraps
 from django.conf import settings
 from django.db import connections
 
+if getattr(settings, 'COSINNUS_USE_WORKER_THREADS', True):
 
-class CosinnusWorkerThread(threading.Thread):
-    """
-    Custom Thread-Class ensuring that DB-Connections are closed when the thread ends.
+    class CosinnusWorkerThread(threading.Thread):
+        """
+        Custom Thread-Class ensuring that DB-Connections are closed when the thread ends.
 
-    - If `COSINNUS_WORKER_THREADS_DISABLE_THREADING` is True, the `run` method is called directly in the main thread.
-    - Ensures, the Thread has a descriptive name including the actual class-name.
-    """
+        - If `COSINNUS_WORKER_THREADS_DISABLE_THREADING` is True, the `run` method is called directly in the main thread
+        - Ensures, the Thread has a descriptive name including the actual class-name.
+        """
 
-    def start(self):
-        if getattr(settings, 'COSINNUS_WORKER_THREADS_DISABLE_THREADING', False):
-            # run in main thread
-            self._original_run()
-            return
-        super().start()
+        def start(self):
+            if getattr(settings, 'COSINNUS_WORKER_THREADS_DISABLE_THREADING', False):
+                # run in main thread
+                self._original_run()
+                return
+            super().start()
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
 
-        if (type_name := type(self).__name__) not in self._name:
-            self._name = f'{type_name}-{self._name}'
+            if (type_name := type(self).__name__) not in self._name:
+                self._name = f'{type_name}-{self._name}'
 
-        self._original_run = self.run
+            self._original_run = self.run
 
-        @wraps(self._original_run)
-        def wrapped_run():
-            try:
-                return self._original_run()
-            finally:
-                connections.close_all()
+            @wraps(self._original_run)
+            def wrapped_run():
+                try:
+                    return self._original_run()
+                finally:
+                    connections.close_all()
 
-        self.run = wrapped_run
+            self.run = wrapped_run
+else:
+
+    class CosinnusWorkerThread(threading.Thread):
+        """Stub-Subclass of vanilla Thread, since WorkerThreads are disabled"""
+
+        pass
