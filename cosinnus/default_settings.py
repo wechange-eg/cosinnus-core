e760c1c9d2d4fce45c1c55cd5420eff141066e09
Jens Möller <jensmoeller.git@gmail.com>
2026-02-26 14:22:34 +0100

    fix: register expiration warning cronjob
    Relates to #1823

--- cosinnus/default_settings.py
+++ cosinnus/default_settings.py
@@ -676,6 +676,7 @@ def define_cosinnus_base_settings(project_settings, project_base_path):
         'cosinnus.cron.DeleteScheduledUserProfiles',
         'cosinnus.cron.UpdateConferencePremiumStatus',
         'cosinnus.cron.SwitchGroupPremiumFeatures',
+        'cosinnus.cron.SendGroupPremiumExpirationWarningEmails',
         'cosinnus.cron.DeleteTemporaryData',
         'cosinnus.cron.SendQueuedMassMails',
         'cosinnus.cron.DeleteOldGuestUsers',
