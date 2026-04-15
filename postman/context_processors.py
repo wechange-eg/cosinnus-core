04421d27d39d55478558d5ebbd3e5fe4409bfaca
Sascha Sommer <saschanarr@gmail.com>
2026-04-15 12:55:50 +0200

    Skip postman inbox processors if postman is not actively used
    Saves a few DB hits, this was running along actively still

--- postman/context_processors.py
+++ postman/context_processors.py
@@ -1,11 +1,17 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
+
 from postman.models import Message
 
 
 def inbox(request):
     """Provide the count of unread messages for an authenticated user."""
-    if request.user.is_authenticated:
-        return {'postman_unread_count': Message.objects.inbox_unread_count(request.user)}
-    else:
-        return {}
+    # only do this if we are actually using it as main messaged module in cosinnus.
+    # skip if rocket is enabled or postman is in archive mode
+    if not getattr(settings, 'COSINNUS_POSTMAN_ARCHIVE_MODE', False) and not getattr(
+        settings, 'COSINNUS_ROCKET_ENABLED', False
+    ):
+        if request.user.is_authenticated:
+            return {'postman_unread_count': Message.objects.inbox_unread_count(request.user)}
+    return {}
