c8d3a0717ad8e6aeac40cfeabc1dc9d10b1043f5
Alexej Starschenko <alexej@2e2a.de>
2026-05-05 14:45:30 +0200

    Change event app label

--- cosinnus_event/cosinnus_app.py
+++ cosinnus_event/cosinnus_app.py
@@ -17,9 +17,8 @@ def register():
     from cosinnus.core.registries import app_registry, attached_object_registry, url_registry, widget_registry
 
     active_by_default = 'cosinnus_event' in settings.COSINNUS_DEFAULT_ACTIVE_GROUP_APPS
-    app_registry.register(
-        'cosinnus_event', 'event', _('Events'), deactivatable=True, active_by_default=active_by_default
-    )
+    app_label = _('Calendar') if settings.COSINNUS_EVENT_V3_CALENDAR_ENABLED else _('Events')
+    app_registry.register('cosinnus_event', 'event', app_label, deactivatable=True, active_by_default=active_by_default)
     attached_object_registry.register('cosinnus_event.Event', 'cosinnus_event.utils.renderer.EventRenderer')
     url_app_name_override = 'calendar' if settings.COSINNUS_EVENT_V3_CALENDAR_ENABLED else None
     url_registry.register_urlconf('cosinnus_event', 'cosinnus_event.urls', url_app_name_override=url_app_name_override)
