45b4f5c2e4060ee96b413c7ea72a6bebd18dee90
Sascha Sommer <saschanarr@gmail.com>
2026-06-03 14:39:00 +0200

    Caldav internal sync: move unthreaded block from cron to sync function

--- cosinnus_event/calendar/cron.py
+++ cosinnus_event/calendar/cron.py
@@ -5,7 +5,6 @@ from django_cron import Schedule
 from cosinnus.conf import settings
 from cosinnus.cron import CosinnusCronJobBase
 from cosinnus.utils.group import get_cosinnus_group_model
-from cosinnus.utils.threading import cosinnus_worker_thread_threading_disabled
 from cosinnus_event.calendar.nextcloud_caldav import NextcloudCaldavConnection
 
 logger = logging.getLogger(__name__)
@@ -27,17 +26,17 @@ class CalendarSyncCaldavEvents(CosinnusCronJobBase):
         groups = get_cosinnus_group_model().objects.filter(is_active=True).exclude(nextcloud_calendar_url=None)
         sync_count = 0
         errors = ''
-        with cosinnus_worker_thread_threading_disabled():
-            for group in groups:
-                try:
-                    error_msg = calendar.group_sync_private_events(group)
-                    sync_count += 1
-                except Exception as e:
-                    error_msg = f'[Group {group.id}] ERROR: Sync failed: {e}'
-                    logger.error(
-                        'CalendarSyncCaldavEvents: Sync of group failed!',
-                        extra={'exception': e, 'group_id': group.id},
-                    )
-                if error_msg:
-                    errors += error_msg + '\n'
+
+        for group in groups:
+            try:
+                error_msg = calendar.group_sync_private_events(group)
+                sync_count += 1
+            except Exception as e:
+                error_msg = f'[Group {group.id}] ERROR: Sync failed: {e}'
+                logger.error(
+                    'CalendarSyncCaldavEvents: Sync of group failed!',
+                    extra={'exception': e, 'group_id': group.id},
+                )
+            if error_msg:
+                errors += error_msg + '\n'
         return f'{sync_count}/{len(groups)} groups synced.' + (f'\n\nErrors/Messages:\n\n{errors}' if errors else '')
