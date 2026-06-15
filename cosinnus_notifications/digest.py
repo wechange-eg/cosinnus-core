5d3ae675b4f6de178c789732ca4b44c254f4b418
Sascha Sommer <saschanarr@gmail.com>
2026-06-15 11:51:17 +0200

    Fixes missing None check for user in NotificationEvents in digests
    Fixes commit f90625f652ebfa8ee7927ab4488c8e94261f683b

--- cosinnus_notifications/digest.py
+++ cosinnus_notifications/digest.py
@@ -278,7 +278,7 @@ def send_digest_for_current_portal(digest_setting, debug_run_for_user=None, debu
                         statecheck = get_requires_object_state_check(event.notification_id)
                         if user == event.user:
                             continue  # users don't receive infos about events they caused
-                        if user is not None and not is_user_active(event.user):
+                        if event.user is not None and not is_user_active(event.user):
                             # users who are inactive by now are probably banned, so ignore their content
                             # if user==None here, we have a generalized notification like from a CalDav event
                             continue
